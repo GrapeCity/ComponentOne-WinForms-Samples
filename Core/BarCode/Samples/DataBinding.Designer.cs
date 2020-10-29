fd3113c069db6730281ebb62ba495ccb782a8e9b
Irina <irina.pykhova@grapecity.com>
2020-10-29 17:48:54 +0500

    corrected BarCode.DataBinding sample for DPI-awareness

--- Core/BarCode/Samples/DataBinding.Designer.cs
+++ Core/BarCode/Samples/DataBinding.Designer.cs
@@ -29,7 +29,6 @@ namespace BarCodeExplorer.Samples
         /// </summary>
         private void InitializeComponent()
         {
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.linkExportTo = new System.Windows.Forms.LinkLabel();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -42,61 +41,27 @@ namespace BarCodeExplorer.Samples
             this.label5 = new System.Windows.Forms.Label();
             this.labelCity = new C1.Win.Input.C1Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
             this.c1BarCode1 = new C1.Win.BarCode.C1BarCode();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.labelAddress = new C1.Win.Input.C1Label();
             this.label1 = new System.Windows.Forms.Label();
             this.labelCustomerName = new C1.Win.Input.C1Label();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.panel11.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
-            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.Gainsboro;
-            this.splitContainer1.Panel1.Controls.Add(this.linkExportTo);
-            this.splitContainer1.Panel1.Controls.Add(this.label4);
-            this.splitContainer1.Panel1.Controls.Add(this.label3);
-            this.splitContainer1.Panel1.Controls.Add(this.button2);
-            this.splitContainer1.Panel1.Controls.Add(this.button1);
-            // 
-            // splitContainer1.Panel2
-            // 
-            this.splitContainer1.Panel2.Controls.Add(this.labelPhone);
-            this.splitContainer1.Panel2.Controls.Add(this.label7);
-            this.splitContainer1.Panel2.Controls.Add(this.label6);
-            this.splitContainer1.Panel2.Controls.Add(this.labelContactName);
-            this.splitContainer1.Panel2.Controls.Add(this.label5);
-            this.splitContainer1.Panel2.Controls.Add(this.labelCity);
-            this.splitContainer1.Panel2.Controls.Add(this.groupBox1);
-            this.splitContainer1.Panel2.Controls.Add(this.label2);
-            this.splitContainer1.Panel2.Controls.Add(this.labelAddress);
-            this.splitContainer1.Panel2.Controls.Add(this.label1);
-            this.splitContainer1.Panel2.Controls.Add(this.labelCustomerName);
-            this.splitContainer1.Size = new System.Drawing.Size(454, 417);
-            this.splitContainer1.SplitterDistance = 28;
-            this.splitContainer1.TabIndex = 0;
             // 
             // linkExportTo
             // 
             this.linkExportTo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.linkExportTo.AutoSize = true;
-            this.linkExportTo.Location = new System.Drawing.Point(354, 6);
+            this.linkExportTo.Location = new System.Drawing.Point(345, 10);
             this.linkExportTo.Name = "linkExportTo";
-            this.linkExportTo.Size = new System.Drawing.Size(90, 15);
+            this.linkExportTo.Size = new System.Drawing.Size(100, 17);
             this.linkExportTo.TabIndex = 4;
             this.linkExportTo.TabStop = true;
             this.linkExportTo.Text = "Export to HTML";
@@ -105,17 +70,17 @@ namespace BarCodeExplorer.Samples
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(208, 6);
+            this.label4.Location = new System.Drawing.Point(211, 10);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 15);
+            this.label4.Size = new System.Drawing.Size(0, 17);
             this.label4.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(162, 6);
+            this.label3.Location = new System.Drawing.Point(165, 10);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.Size = new System.Drawing.Size(43, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "label3";
             // 
@@ -123,7 +88,7 @@ namespace BarCodeExplorer.Samples
             // 
             this.button2.Location = new System.Drawing.Point(81, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(75, 26);
             this.button2.TabIndex = 1;
             this.button2.Text = "Next";
             this.button2.UseVisualStyleBackColor = true;
@@ -133,7 +98,7 @@ namespace BarCodeExplorer.Samples
             // 
             this.button1.Location = new System.Drawing.Point(4, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 26);
             this.button1.TabIndex = 0;
             this.button1.Text = "Prev";
             this.button1.UseVisualStyleBackColor = true;
@@ -143,27 +108,27 @@ namespace BarCodeExplorer.Samples
             // 
             this.labelPhone.AutoSize = true;
             this.labelPhone.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.labelPhone.Location = new System.Drawing.Point(114, 107);
+            this.labelPhone.Location = new System.Drawing.Point(114, 121);
             this.labelPhone.Name = "labelPhone";
-            this.labelPhone.Size = new System.Drawing.Size(331, 21);
+            this.labelPhone.Size = new System.Drawing.Size(331, 24);
             this.labelPhone.TabIndex = 10;
             this.labelPhone.Text = "labelPhone";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(14, 108);
+            this.label7.Location = new System.Drawing.Point(14, 122);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(44, 15);
+            this.label7.Size = new System.Drawing.Size(47, 17);
             this.label7.TabIndex = 11;
             this.label7.Text = "Phone:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(14, 81);
+            this.label6.Location = new System.Drawing.Point(14, 92);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(85, 15);
+            this.label6.Size = new System.Drawing.Size(91, 17);
             this.label6.TabIndex = 9;
             this.label6.Text = "Contact name:";
             // 
@@ -171,18 +136,18 @@ namespace BarCodeExplorer.Samples
             // 
             this.labelContactName.AutoSize = true;
             this.labelContactName.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.labelContactName.Location = new System.Drawing.Point(114, 80);
+            this.labelContactName.Location = new System.Drawing.Point(114, 91);
             this.labelContactName.Name = "labelContactName";
-            this.labelContactName.Size = new System.Drawing.Size(331, 21);
+            this.labelContactName.Size = new System.Drawing.Size(331, 24);
             this.labelContactName.TabIndex = 8;
             this.labelContactName.Text = "labelContactName";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(14, 56);
+            this.label5.Location = new System.Drawing.Point(14, 63);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(31, 15);
+            this.label5.Size = new System.Drawing.Size(32, 17);
             this.label5.TabIndex = 7;
             this.label5.Text = "City:";
             // 
@@ -190,32 +155,32 @@ namespace BarCodeExplorer.Samples
             // 
             this.labelCity.AutoSize = true;
             this.labelCity.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.labelCity.Location = new System.Drawing.Point(114, 55);
+            this.labelCity.Location = new System.Drawing.Point(114, 62);
             this.labelCity.Name = "labelCity";
-            this.labelCity.Size = new System.Drawing.Size(331, 21);
+            this.labelCity.Size = new System.Drawing.Size(331, 24);
             this.labelCity.TabIndex = 6;
             this.labelCity.Text = "labelCity";
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.panel1);
-            this.groupBox1.Location = new System.Drawing.Point(9, 139);
+            this.groupBox1.Controls.Add(this.panel11);
+            this.groupBox1.Location = new System.Drawing.Point(9, 158);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(173, 193);
+            this.groupBox1.Size = new System.Drawing.Size(173, 219);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Postal code";
             // 
-            // panel1
+            // panel11
             // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.panel11.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Controls.Add(this.c1BarCode1);
-            this.panel1.Location = new System.Drawing.Point(7, 23);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(163, 164);
-            this.panel1.TabIndex = 0;
+            this.panel11.Controls.Add(this.c1BarCode1);
+            this.panel11.Location = new System.Drawing.Point(7, 26);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(163, 186);
+            this.panel11.TabIndex = 0;
             // 
             // c1BarCode1
             // 
@@ -232,18 +197,53 @@ namespace BarCodeExplorer.Samples
             this.c1BarCode1.QuietZone.Left = 0D;
             this.c1BarCode1.QuietZone.Right = 0D;
             this.c1BarCode1.QuietZone.Top = 0D;
-            this.c1BarCode1.Size = new System.Drawing.Size(163, 164);
+            this.c1BarCode1.Size = new System.Drawing.Size(163, 186);
             this.c1BarCode1.TabIndex = 0;
             this.c1BarCode1.Text = "c1BarCode1";
             this.c1BarCode1.TextFixedLength = 0;
             this.c1BarCode1.WideToNarrowRatio = 2F;
             // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.linkExportTo);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(3);
+            this.panel1.Size = new System.Drawing.Size(454, 34);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.labelPhone);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.labelContactName);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.labelCity);
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.labelAddress);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.labelCustomerName);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 34);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(454, 439);
+            this.panel2.TabIndex = 0;
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 30);
+            this.label2.Location = new System.Drawing.Point(14, 34);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.Size = new System.Drawing.Size(59, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Address:";
             // 
@@ -251,18 +251,18 @@ namespace BarCodeExplorer.Samples
             // 
             this.labelAddress.AutoSize = true;
             this.labelAddress.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.labelAddress.Location = new System.Drawing.Point(114, 29);
+            this.labelAddress.Location = new System.Drawing.Point(114, 33);
             this.labelAddress.Name = "labelAddress";
-            this.labelAddress.Size = new System.Drawing.Size(331, 21);
+            this.labelAddress.Size = new System.Drawing.Size(331, 24);
             this.labelAddress.TabIndex = 3;
             this.labelAddress.Text = "labelAddress";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 5);
+            this.label1.Location = new System.Drawing.Point(14, 6);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.Size = new System.Drawing.Size(103, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "Customer name:";
             // 
@@ -270,42 +270,43 @@ namespace BarCodeExplorer.Samples
             // 
             this.labelCustomerName.AutoSize = true;
             this.labelCustomerName.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.labelCustomerName.Location = new System.Drawing.Point(114, 4);
+            this.labelCustomerName.Location = new System.Drawing.Point(114, 5);
             this.labelCustomerName.Name = "labelCustomerName";
-            this.labelCustomerName.Size = new System.Drawing.Size(331, 21);
+            this.labelCustomerName.Size = new System.Drawing.Size(331, 24);
             this.labelCustomerName.TabIndex = 0;
             this.labelCustomerName.Text = "labelCustomerName";
             // 
             // DataBinding
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.splitContainer1);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "DataBinding";
-            this.Size = new System.Drawing.Size(454, 417);
+            this.Size = new System.Drawing.Size(454, 473);
             this.Load += new System.EventHandler(this.DataBound_Load);
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel1.PerformLayout();
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            this.splitContainer1.Panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
+            this.panel11.ResumeLayout(false);
+            this.panel11.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel11;
         private System.Windows.Forms.Label label1;
         private C1.Win.Input.C1Label labelCustomerName;
         private System.Windows.Forms.Label label2;
         private C1.Win.Input.C1Label labelAddress;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
         private C1.Win.BarCode.C1BarCode c1BarCode1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
--- Core/BarCode/Samples/Overview.Designer.cs
+++ Core/BarCode/Samples/Overview.Designer.cs
@@ -53,7 +53,7 @@ namespace BarCodeExplorer.Samples
             this._panel.Dock = System.Windows.Forms.DockStyle.Fill;
             this._panel.Location = new System.Drawing.Point(0, 0);
             this._panel.Name = "_panel";
-            this._panel.Size = new System.Drawing.Size(300, 371);
+            this._panel.Size = new System.Drawing.Size(300, 424);
             this._panel.TabIndex = 0;
             // 
             // splitContainer1
@@ -71,7 +71,7 @@ namespace BarCodeExplorer.Samples
             // 
             this.splitContainer1.Panel2.Controls.Add(this._settings);
             this.splitContainer1.Panel2MinSize = 100;
-            this.splitContainer1.Size = new System.Drawing.Size(519, 405);
+            this.splitContainer1.Size = new System.Drawing.Size(519, 459);
             this.splitContainer1.SplitterDistance = 300;
             this.splitContainer1.TabIndex = 7;
             // 
@@ -94,8 +94,9 @@ namespace BarCodeExplorer.Samples
             // splitContainer2.Panel2
             // 
             this.splitContainer2.Panel2.Controls.Add(this._panel);
-            this.splitContainer2.Size = new System.Drawing.Size(300, 405);
+            this.splitContainer2.Size = new System.Drawing.Size(300, 459);
             this.splitContainer2.SplitterDistance = 30;
+            this.splitContainer2.SplitterWidth = 5;
             this.splitContainer2.TabIndex = 1;
             // 
             // _quantity
@@ -103,9 +104,9 @@ namespace BarCodeExplorer.Samples
             this._quantity.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this._quantity.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this._quantity.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this._quantity.Location = new System.Drawing.Point(260, 6);
+            this._quantity.Location = new System.Drawing.Point(260, 7);
             this._quantity.Name = "_quantity";
-            this._quantity.Size = new System.Drawing.Size(33, 18);
+            this._quantity.Size = new System.Drawing.Size(33, 20);
             this._quantity.TabIndex = 3;
             this._quantity.Text = "0";
             // 
@@ -113,9 +114,9 @@ namespace BarCodeExplorer.Samples
             // 
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(198, 8);
+            this.label2.Location = new System.Drawing.Point(198, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 15);
+            this.label2.Size = new System.Drawing.Size(59, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Quantity:";
             // 
@@ -130,16 +131,16 @@ namespace BarCodeExplorer.Samples
             "All"});
             this._mode.Location = new System.Drawing.Point(85, 3);
             this._mode.Name = "_mode";
-            this._mode.Size = new System.Drawing.Size(107, 23);
+            this._mode.Size = new System.Drawing.Size(107, 25);
             this._mode.TabIndex = 1;
             this._mode.SelectedIndexChanged += new System.EventHandler(this._mode_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 7);
+            this.label1.Location = new System.Drawing.Point(4, 8);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 15);
+            this.label1.Size = new System.Drawing.Size(83, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Select mode:";
             // 
@@ -151,17 +152,17 @@ namespace BarCodeExplorer.Samples
             this._settings.Location = new System.Drawing.Point(0, 0);
             this._settings.Name = "_settings";
             this._settings.PropertySort = System.Windows.Forms.PropertySort.Alphabetical;
-            this._settings.Size = new System.Drawing.Size(215, 405);
+            this._settings.Size = new System.Drawing.Size(215, 459);
             this._settings.TabIndex = 5;
             this._settings.ToolbarVisible = false;
             // 
             // Overview
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.Controls.Add(this.splitContainer1);
             this.Name = "Overview";
-            this.Size = new System.Drawing.Size(519, 405);
+            this.Size = new System.Drawing.Size(519, 459);
             this.Load += new System.EventHandler(this.Overview_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
