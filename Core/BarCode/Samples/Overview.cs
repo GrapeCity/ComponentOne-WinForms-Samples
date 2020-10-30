634ea50934a74aa535ced8daec509db29e8c4d06
Nikita Parkhomenko <Nikita.Parkhomenko@grapecity.com>
2020-10-30 12:50:16 +0300

    fixed C1WIN-23795: [Net5][Samples] Added barcode scaling.

--- Core/BarCode/Samples/Overview.cs
+++ Core/BarCode/Samples/Overview.cs
@@ -62,14 +62,11 @@ namespace BarCodeExplorer.Samples
 
             var list = result
                 .OrderBy(x => x)
-                .Select(x => new PanelBarCode(x)
-                {
-                    // QR
-                    Size = _mode.SelectedIndex == 1 ? new Size(200, 200) :
-                    // Popular
-                           _mode.SelectedIndex == 0 ? new Size(200, 80) :
-                            new Size(200, 70)
-                })
+                .Select(x => new PanelBarCode(x, _mode.SelectedIndex != 1,
+                     _mode.SelectedIndex == 1 ? new Size(200, 200) :// QR                                                                   
+                     _mode.SelectedIndex == 0 ? new Size(200, 80) :// Popular
+                     new Size(200, 70)                     
+                ))
                .ToList();
 
             _quantity.Text = list.Count().ToString();
