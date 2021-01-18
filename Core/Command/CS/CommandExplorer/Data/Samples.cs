81cf04b332747cd876742f84a19ea0013cb1afcd
Alexander Rakhmanberdiev <alexander.rakhmanberdiev@grapecity.com>
2021-01-18 12:40:49 +0300

    C1WIN-24048: [C1Command][C1DockingTab] Added .Net 5 "CommandExplorer" sample

---
+++ Core/Command/CS/CommandExplorer/Data/Samples.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Forms;
+
+namespace CommandExplorer
+{
+    public class SampleItem
+    {
+        public SampleItem(string name, string title, string desc, Control sample)
+        {
+            Name = name;
+            Title = title;
+            Description = desc;
+            Sample = sample;
+        }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Control Sample { get; set; }
+    }
+
+    public static class SampleDataSource
+    {
+        private static List<SampleItem> _allItems = new List<SampleItem>();
+
+        static SampleDataSource()
+        {
+            // add SampleItem for each sample you want to show
+            _allItems.Add(new SampleItem("DockingOverview",
+               "Docking Overview",
+               "The C1DockingTab control implements the familiar tab control interface with enhanced appearance and behavioral settings.", 
+               new Samples.DockingOverview()));
+            _allItems.Add(new SampleItem("Docking",
+                "Docking",
+                "C1DockingTabs can float and dock to any edge of the application. Choose among four different docking styles, including Visual Studio one.", // Float and dock tabs to create different workspaces
+                new Samples.Docking()));
+            _allItems.Add(new SampleItem("ToolbarsOverview",
+               "Toolbars and Menus",
+               "This demo shows C1Menu, C1ContextMenu and C1Toolbar which share the same set of commands.",
+               new Samples.ToolbarsOverview()));
+        }
+
+        public static IList<SampleItem> AllItems
+        {
+            get { return _allItems; }
+        }
+    }
+}
---
+++ Core/Command/CS/CommandExplorer/Form1.cs
@@ -0,0 +1,76 @@
+﻿using C1.Win.Themes;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CommandExplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            foreach (SampleItem sample in SampleDataSource.AllItems)
+            {
+                lblSamples.Items.Add(sample);
+            }
+
+            lblSamples.SelectedIndex = 0;
+
+            var item = new C1.Win.Input.ComboBoxItem();
+            item.DisplayText = "(none)";
+            cmbThemes.Items.Add(item);
+            
+            var themes = C1ThemeController.GetThemes();
+            
+            foreach(var theme in themes)
+            {
+                var themeItem = new C1.Win.Input.ComboBoxItem();
+                themeItem.DisplayText = theme;
+                cmbThemes.Items.Add(themeItem);
+            }
+            
+            cmbThemes.SelectedIndex = 0;
+        }
+
+        private void lbSamples_SelectedValueChanged(object sender, EventArgs e)
+        {
+            pnlSample.Controls.Clear();
+            var sample = lblSamples.SelectedItem as SampleItem;
+            lblTitle.Text = sample.Title;
+            lblDescription.Text = sample.Description;
+            var control = sample.Sample;
+            control.Dock = DockStyle.Fill;
+            pnlSample.Controls.Add(control);
+
+            if (control is Form)
+            {
+                ((Form)control).Show();
+            }
+        }
+
+        private void chkInfo_CheckedChanged(object sender, EventArgs e)
+        {
+            pnlDescription.Visible = chkInfo.Checked;
+        }
+
+        private void cmbThemes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbThemes.SelectedIndex == -1)
+            {
+                return;
+            }
+
+            var value = cmbThemes.SelectedItem.DisplayText;
+            cmbThemes.Text = value;
+            C1ThemeController.ApplyThemeToControlTree(pnlSample, C1ThemeController.GetThemeByName(value, false), null, true);
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            lblDescription.MaximumSize = new Size(pnlDescription.Width, 0);
+        }
+    }
+}
