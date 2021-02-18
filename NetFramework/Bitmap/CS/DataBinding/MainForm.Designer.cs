f9724b56a0692b26ce42e9361d09c70e7b01cd47
Nikita Parkhomenko <Nikita.Parkhomenko@grapecity.com>
2021-02-18 11:44:58 +0300

    [Samples[NET452]] Added Bitmap DataBinding sample.

---
+++ NetFramework/Bitmap/CS/DataBinding/MainForm.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace DataBinding
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.c1Ribbon1 = new C1.Win.Ribbon.C1Ribbon();
+            this.ribbonApplicationMenu1 = new C1.Win.Ribbon.RibbonApplicationMenu();
+            this.ribbonBottomToolBar1 = new C1.Win.Ribbon.RibbonBottomToolBar();
+            this.ribbonConfigToolBar1 = new C1.Win.Ribbon.RibbonConfigToolBar();
+            this.ribbonQat1 = new C1.Win.Ribbon.RibbonQat();
+            this.ribbonTab1 = new C1.Win.Ribbon.RibbonTab();
+            this.ribbonGroup1 = new C1.Win.Ribbon.RibbonGroup();
+            this.cmbTheme = new C1.Win.Ribbon.RibbonComboBox();
+            this.ribbonTopToolBar1 = new C1.Win.Ribbon.RibbonTopToolBar();
+            this.c1ThemeController1 = new C1.Win.C1Themes.C1ThemeController();
+            this.contentPanel = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabelRefresh = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.c1PictureBox1 = new C1.Win.C1Input.C1PictureBox();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.contentPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.c1PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1Ribbon1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1ThemeController1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // c1Ribbon1
+            // 
+            this.c1Ribbon1.ApplicationMenuHolder = this.ribbonApplicationMenu1;
+            this.c1Ribbon1.AutoSizeElement = C1.Framework.AutoSizeElement.Width;
+            this.c1Ribbon1.BottomToolBarHolder = this.ribbonBottomToolBar1;
+            this.c1Ribbon1.ConfigToolBarHolder = this.ribbonConfigToolBar1;
+            this.c1Ribbon1.HideTabHeaderRow = true;
+            this.c1Ribbon1.Location = new System.Drawing.Point(0, 0);
+            this.c1Ribbon1.Name = "c1Ribbon1";
+            this.c1Ribbon1.QatHolder = this.ribbonQat1;
+            this.c1Ribbon1.Size = new System.Drawing.Size(800, 77);
+            this.c1Ribbon1.Tabs.Add(this.ribbonTab1);
+            this.c1ThemeController1.SetTheme(this.c1Ribbon1, "(default)");
+            this.c1Ribbon1.TopToolBarHolder = this.ribbonTopToolBar1;
+            this.c1Ribbon1.ViewMode = C1.Win.Ribbon.ViewMode.Simplified;
+            // 
+            // ribbonApplicationMenu1
+            // 
+            this.ribbonApplicationMenu1.Name = "ribbonApplicationMenu1";
+            this.ribbonApplicationMenu1.Visible = false;
+            // 
+            // ribbonBottomToolBar1
+            // 
+            this.ribbonBottomToolBar1.Name = "ribbonBottomToolBar1";
+            // 
+            // ribbonConfigToolBar1
+            // 
+            this.ribbonConfigToolBar1.Name = "ribbonConfigToolBar1";
+            // 
+            // ribbonQat1
+            // 
+            this.ribbonQat1.Name = "ribbonQat1";
+            this.ribbonQat1.Visible = false;
+            // 
+            // ribbonTab1
+            // 
+            this.ribbonTab1.Groups.Add(this.ribbonGroup1);
+            this.ribbonTab1.Name = "ribbonTab1";
+            this.ribbonTab1.Text = "Tab";
+            // 
+            // ribbonGroup1
+            // 
+            this.ribbonGroup1.Items.Add(this.cmbTheme);
+            this.ribbonGroup1.Name = "ribbonGroup1";
+            this.ribbonGroup1.Text = "Styles";
+            // 
+            // cmbTheme
+            // 
+            this.cmbTheme.Label = "Theme";
+            this.cmbTheme.Name = "cmbTheme";
+            this.cmbTheme.SelectedIndexChanged += new System.EventHandler(this.cmbTheme_SelectedIndexChanged);
+            // 
+            // ribbonTopToolBar1
+            // 
+            this.ribbonTopToolBar1.Name = "ribbonTopToolBar1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.linkLabelRefresh);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(425, 41);
+            this.panel1.TabIndex = 0;
+            // 
+            // linkLabelRefresh
+            // 
+            this.linkLabelRefresh.AutoSize = true;
+            this.linkLabelRefresh.Location = new System.Drawing.Point(337, 14);
+            this.linkLabelRefresh.Name = "linkLabelRefresh";
+            this.linkLabelRefresh.Size = new System.Drawing.Size(46, 15);
+            this.linkLabelRefresh.TabIndex = 4;
+            this.linkLabelRefresh.TabStop = true;
+            this.linkLabelRefresh.Text = "Refresh";
+            this.linkLabelRefresh.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelRefresh_LinkClicked);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(270, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search text:";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.richTextBox1);
+            this.panel2.Controls.Add(this.c1PictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(202, 41);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(223, 373);
+            this.panel2.TabIndex = 4;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(7, 162);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(209, 97);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // c1PictureBox1
+            // 
+            this.c1PictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.c1PictureBox1.Location = new System.Drawing.Point(7, 7);
+            this.c1PictureBox1.Name = "c1PictureBox1";
+            this.c1PictureBox1.Size = new System.Drawing.Size(209, 149);
+            this.c1PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.c1PictureBox1.TabIndex = 0;
+            this.c1PictureBox1.TabStop = false;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.splitter1.Location = new System.Drawing.Point(199, 41);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 373);
+            this.splitter1.TabIndex = 5;
+            this.splitter1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 41);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(199, 373);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // contentPanel
+            // 
+            this.contentPanel.Controls.Add(this.flowLayoutPanel1);
+            this.contentPanel.Controls.Add(this.splitter1);
+            this.contentPanel.Controls.Add(this.panel2);
+            this.contentPanel.Controls.Add(this.panel1);
+            this.contentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.contentPanel.Location = new System.Drawing.Point(0, 77);
+            this.contentPanel.Name = "panel1";
+            this.contentPanel.Size = new System.Drawing.Size(800, 373);
+            this.contentPanel.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.contentPanel);
+            this.Controls.Add(this.c1Ribbon1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DataBinding";
+            this.Text = "Form1";
+            this.c1ThemeController1.SetTheme(this, "(default)");
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.c1Ribbon1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1ThemeController1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private C1.Win.Ribbon.C1Ribbon c1Ribbon1;
+        private C1.Win.Ribbon.RibbonApplicationMenu ribbonApplicationMenu1;
+        private C1.Win.Ribbon.RibbonBottomToolBar ribbonBottomToolBar1;
+        private C1.Win.Ribbon.RibbonConfigToolBar ribbonConfigToolBar1;
+        private C1.Win.Ribbon.RibbonQat ribbonQat1;
+        private C1.Win.Ribbon.RibbonTab ribbonTab1;
+        private C1.Win.Ribbon.RibbonGroup ribbonGroup1;
+        private C1.Win.Ribbon.RibbonTopToolBar ribbonTopToolBar1;
+        private C1.Win.Ribbon.RibbonComboBox cmbTheme;
+        private C1.Win.C1Themes.C1ThemeController c1ThemeController1;
+        private System.Windows.Forms.Panel contentPanel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private C1.Win.C1Input.C1PictureBox c1PictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.LinkLabel linkLabelRefresh;
+    }
+}
+
---
+++ NetFramework/Bitmap/CS/DataBinding/MainForm.cs
@@ -0,0 +1,123 @@
+﻿using C1.Win.C1Themes;
+using C1.Win.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBinding
+{
+    public partial class MainForm : C1RibbonForm
+    {
+        private static Random rnd = new Random();
+        private List<BitmapPanel> _items;
+        private BitmapPanel _selectPanel;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            InitThemes();
+            if (!DesignMode)
+                InitializeBitmaps();
+        }
+
+        private void InitializeBitmaps()
+        {
+            var request =  "Select Title as Caption, Photo as Picture, FirstName as Description from Employees;"; 
+
+            var data = GetDataSource(request);
+
+            // Set random order positions
+            var count = data.Rows.Count;
+            var list = new List<int>(Enumerable.Range(0, count + 1).ToArray());
+
+            // Creating pictures
+            _items = data.Rows.Cast<DataRow>()
+                .Select(x => new BitmapPanel(x, true, new Size(150, 150)))
+                .ToList();
+            _items.ForEach(x => x.SelectImage += Image_Click);
+
+            flowLayoutPanel1.Controls.Clear();
+            flowLayoutPanel1.Controls.AddRange(_items.ToArray());
+        }
+
+        private void Image_Click(object sender, EventArgs e)
+        {
+            var panel = sender as BitmapPanel;
+            if (panel == null) return;
+
+            // Changed select flag
+            if (_selectPanel != null)
+                _selectPanel.IsSelect = false;
+            panel.IsSelect = true;
+
+            // Get and transformed picture
+            _selectPanel = panel;
+            c1PictureBox1.Image = Effects.RoundUpImage(panel.Bitmap).ToGdiBitmap();
+            richTextBox1.Text = panel.Description;
+        }
+
+        private void linkLabelRefresh_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            textBox1.Text = "";
+            InitializeBitmaps();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var searchText = textBox1.Text.Trim();
+            var result = _items.Where(x => x.Caption.IndexOf(searchText) >= 0);
+            if (result.Any())
+            {
+                _items.ToList()
+                    .ForEach(x => x.Select(searchText));
+            }
+        }
+
+        #region Themes
+
+        private void InitThemes()
+        {
+            var themes = C1ThemeController.GetThemes().Where(x => x.Contains("Office2016"));
+            foreach (var theme in themes)
+                cmbTheme.Items.Add(theme);
+        }
+
+        private void cmbTheme_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            c1ThemeController1.Theme = cmbTheme.SelectedItem.Text;
+        }
+
+        #endregion
+
+        #region DataSource
+
+        private DataTable GetDataSource(string request)
+        {
+            var cn = GetConnectionString();
+            var da = new OleDbDataAdapter(request, cn);
+            var dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        static string GetConnectionString()
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + @"\ComponentOne Samples\Common";
+            var conn = @"provider=microsoft.jet.oledb.4.0;data source={0}\c1nwind.mdb;";
+            return string.Format(conn, path);
+        }
+
+        #endregion
+    }
+}
