f609e6c88f94f95861ae44a869aff101f4264dfe
KonstantinPletnev <konstantin.pletnev@grapecity.com>
2021-03-22 17:24:23 +0300

    [DataFilter] Added DataFilter explorer.

---
+++ Core/DataFilterExplorer/Data/DataService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace ChartAsDataFilter.Data
+{
+    public class DataService
+    {
+        private static List<Quote> dt;
+        static DataService()
+        {
+            var buildDir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var quotesPath = Path.Combine(buildDir, "Resources", "HistoricalQuotes.json");
+            var text = File.ReadAllText(quotesPath);
+            dt = JsonSerializer.Deserialize<List<Quote>>(text);
+        }        
+
+        public static List<Quote> GetFinancialData(int count = 365)
+        {
+            count = Math.Min(count, dt.Count);
+            return dt.Take(count).OrderBy(x => x.Date).ToList();
+        }
+
+        public static List<Quote> GetFinancialData(DateTime startDate, int count = 365)
+        {
+            var quotes = GetFinancialData(count);
+            foreach (var quote in quotes)
+            {
+                quote.Date = startDate.AddDays(quotes.IndexOf(quote));
+            }
+            return quotes;
+        }
+    }
+}
---
+++ Core/DataFilterExplorer/Samples/Controls/DropDownFilter.cs
@@ -0,0 +1,63 @@
+﻿using C1.DataCollection;
+using C1.DataFilter;
+using C1.Win.DataFilter;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DataFilterExplorer.Samples
+{
+    public class DropDownFilter : ToolStripDropDown
+    {
+        public DropDownFilter()
+        {
+            AutoClose = true;
+            AutoSize = true;
+            Margin = Padding.Empty;
+            Padding = Padding.Empty;
+
+            DataFilter = new C1DataFilter
+            {
+                AutoWidthMode = AutoWidthMode.Grow,
+                ShowClearFilterButtons = true,
+                ShowHeader = false
+            };
+
+            var host = new ToolStripControlHost(DataFilter)
+            {
+                Margin = Padding.Empty,
+                Padding = Padding.Empty,
+                AutoSize = true,
+                AutoToolTip = false
+            };
+
+            Items.Clear();
+            Items.Add(host);
+        }
+
+        public void ShowFilter(string propertyName, Control control, Point location)
+        {
+            var isFound = false;
+            foreach (var filter in DataFilter.Filters)
+            {
+                if (filter.PropertyName == propertyName)
+                {
+                    filter.Expanded = true;
+                    isFound = true;
+                }
+                else
+                    filter.Expanded = false;
+            }
+            if (isFound)
+            {
+                DataFilter.Height = GetPreferredSize(Size.Empty).Height;
+                Show(control, location);
+            }
+        }
+
+        public void AddFilter(Filter filter) => DataFilter.Filters.Add(filter);
+
+        public void ApplyFilter(ISupportFiltering dataSource) => dataSource.FilterAsync(DataFilter.GetFilterExpression());
+
+        public C1DataFilter DataFilter { get; }
+    }
+}
