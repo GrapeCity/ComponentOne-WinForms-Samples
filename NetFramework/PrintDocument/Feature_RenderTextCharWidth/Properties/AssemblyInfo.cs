1a2caefcdbb987e2dcd55e6adae79f222fa75f66
Irina <irina.pykhova@grapecity.com>
2021-05-05 21:58:17 +0500

    printing for 4.5.2

--- NetFramework/PrintDocument/Feature_RenderTextCharWidth/Properties/AssemblyInfo.cs
+++ NetFramework/PrintDocument/Feature_RenderTextCharWidth/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Feature_TestRenderTextCharWidth")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("*")]
 [assembly: AssemblyProduct("Feature_TestRenderTextCharWidth")]
-[assembly: AssemblyCopyright("Copyright © * 2008")]
+[assembly: AssemblyDescription("ComponentOne Studio WinForms Edition")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GrapeCity, Inc.")]
+[assembly: AssemblyCopyright("(c) GrapeCity, Inc. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
