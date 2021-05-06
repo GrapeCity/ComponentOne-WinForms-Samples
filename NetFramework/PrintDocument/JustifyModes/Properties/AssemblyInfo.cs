1693cadcbb965464218b20732ed290f73ea6b690
Irina <irina.pykhova@grapecity.com>
2021-05-06 17:51:14 +0500

    printing for 4.5.2

--- NetFramework/PrintDocument/JustifyModes/Properties/AssemblyInfo.cs
+++ NetFramework/PrintDocument/JustifyModes/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JustifyModes")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("*")]
 [assembly: AssemblyProduct("JustifyModes")]
-[assembly: AssemblyCopyright("Copyright © * 2008")]
+[assembly: AssemblyDescription("ComponentOne Studio WinForms Edition")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GrapeCity, Inc.")]
+[assembly: AssemblyCopyright("(c) GrapeCity, Inc. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
