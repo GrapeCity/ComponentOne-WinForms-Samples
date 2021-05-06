b36bd88a65b169f1369c5c04c7ea1f7a04718845
Irina <irina.pykhova@grapecity.com>
2021-05-06 22:02:40 +0500

    printing for 4.5.2

--- NetFramework/PrintDocument/PageLayout1/Properties/AssemblyInfo.cs
+++ NetFramework/PrintDocument/PageLayout1/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PageLayout")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Fantasy")]
 [assembly: AssemblyProduct("PageLayout")]
-[assembly: AssemblyCopyright("Copyright © Fantasy 2005")]
+[assembly: AssemblyDescription("ComponentOne Studio WinForms Edition")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GrapeCity, Inc.")]
+[assembly: AssemblyCopyright("(c) GrapeCity, Inc. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
