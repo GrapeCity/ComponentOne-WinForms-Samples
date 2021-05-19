c683c8c42fe1a36962a120691748f5d35327aaa6
Maria Korchemkina <maria.korchemkina@grapecity.com>
2021-05-19 10:11:56 +0000

    Merged in feature/C1WIN-24805---Add-sample-for-C1WinInputPanel-to-ControlExplorer-4.5.2 (pull request #53)
    Feature/C1WIN-24805   Add sample for C1WinInputPanel to ControlExplorer 4.5.2
    
    * C1WIN-24805 - Add BillOfSale and FlowPanelCatalogue samples sample for InputPanel to ControlExplorer 4.5.2
    
    * C1WIN-24805 - fix bill of sale and catalogue samples for input panel
    
    * resolve merge conflict

---
+++ NetFramework/WinForms/CS/ControlExplorer/InputPanel/BillOfSales.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlExplorer.InputPanel
+{
+    public partial class BillOfSales : C1DemoForm
+    {
+        public BillOfSales()
+        {
+            InitializeComponent();
+        }
+
+        private void BillOfSales_Load(object sender, EventArgs e)
+        {
+            BindingList<SaleInfo> datalist = new BindingList<SaleInfo>();
+
+            Transactor t1 = new Transactor("Alice Smit", "Wonderland", "Tinytown", "S1", "89182", "222222", "987654321021", "ABC", new DateTime(2021, 4, 6));
+            Transactor t2 = new Transactor("Bob Bill", "Oz", "Largetown", "S2", "98064", "333333", "123456789012", "Technology inc", new DateTime(2021, 4, 2));
+            datalist.Add(new SaleInfo(new DateTime(2021, 4, 6), t1, t2, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel tortor dolor. Fusce vulputate facilisis tortor commodo luctus. Praesent ligula ante, convallis eget lacus ut, ultrices semper enim. Morbi ac scelerisque metus. Integer finibus malesuada fermentum. Donec pulvinar nisl nec felis ornare, eu egestas augue molestie. Donec sed dui sit amet enim pulvinar blandit. In ac dui bibendum, maximus diam eu, tempus nulla. Aliquam pellentesque viverra felis hendrerit dictum. Sed pharetra nisl non leo tincidunt feugiat.",
+                300, "cash"));
+            billOfSaleBindingSource.DataSource = datalist[0];
+        }
+
+
+        public class SaleInfo
+        {
+            public DateTime DateSold { get; set; }
+            public Transactor Seller { get; set; }
+            public Transactor Buyer { get; set; }
+            public string ItemsInfo { get; set; }
+            public int ItemsValue { get; set; }
+            public string FormOfPayment { get; set; }
+            public SaleInfo(DateTime dateS, Transactor s, Transactor b, string itemsInfo, int itemsValue, string formPayment)
+            {
+                DateSold = dateS;
+                Seller = s;
+                Buyer = b;
+                ItemsInfo = itemsInfo;
+                ItemsValue = itemsValue;
+                FormOfPayment = formPayment;
+            }
+
+        }
+        public struct Transactor
+        {
+            public string PrintedName { get; set; }
+            public string Address { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string Zip { get; set; }
+            public string HomePhone { get; set; }
+            public string CellPhone { get; set; }
+            public string Work { get; set; }
+            public DateTime DateSign { get; set; }
+
+            public Transactor(string printName, string adress, string city, string state, string zip,
+                string homePhone, string cellPhone, string work, DateTime dateSign)
+            {
+
+                PrintedName = printName;
+                Address = adress;
+                City = city;
+                State = state;
+                Zip = zip;
+                HomePhone = homePhone;
+                CellPhone = cellPhone;
+                Work = work;
+                DateSign = dateSign;
+            }
+
+        }
+    }
+}
---
+++ NetFramework/WinForms/CS/ControlExplorer/InputPanel/FlowPanelCatalogue.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace ControlExplorer.InputPanel
+{
+    partial class FlowPanelCatalogue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.c1InputPanel1 = new C1.Win.C1InputPanel.C1InputPanel();
+            this.inputFlowPanel1 = new C1.Win.C1InputPanel.InputFlowPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.c1InputPanel1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // c1InputPanel1
+            // 
+            this.c1InputPanel1.AutoSizeElement = C1.Framework.AutoSizeElement.Both;
+            this.c1InputPanel1.Dock = System.Windows.Forms.DockStyle.Fill; 
+            this.c1InputPanel1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.c1InputPanel1.Items.Add(this.inputFlowPanel1);
+            this.c1InputPanel1.Location = new System.Drawing.Point(0, 0);
+            this.c1InputPanel1.Name = "c1InputPanel1";
+            this.c1InputPanel1.Size = new System.Drawing.Size(890, 500);
+            this.c1InputPanel1.TabIndex = 4;
+            // 
+            // inputFlowPanel1
+            // 
+            this.inputFlowPanel1.ChildSpacing = new System.Drawing.Size(15, 15);
+            this.inputFlowPanel1.Height = -1;
+            this.inputFlowPanel1.Name = "inputFlowPanel1";
+            // 
+            // FlowPanelСatalogue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            //this.ClientSize = new System.Drawing.Size(1302, 331);
+            this.Controls.Add(this.c1InputPanel1);
+            this.Name = "FlowPanelСatalogue";
+            this.Text = "Catalogue";
+            this.Load += new System.EventHandler(this.FlowPanelСatalogue_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.c1InputPanel1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private C1.Win.C1InputPanel.C1InputPanel c1InputPanel1;
+        private C1.Win.C1InputPanel.InputFlowPanel inputFlowPanel1;
+
+    }
+}
