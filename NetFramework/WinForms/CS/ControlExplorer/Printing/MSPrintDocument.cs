fe26ee7fcac1f805943e208df58206fb907392fa
Irina <irina.pykhova@grapecity.com>
2021-05-20 19:31:19 +0500

    refactored File Viewer sample, added new status for Printing

--- NetFramework/WinForms/CS/ControlExplorer/Printing/MSPrintDocument.cs
+++ NetFramework/WinForms/CS/ControlExplorer/Printing/MSPrintDocument.cs
@@ -13,24 +13,101 @@ namespace ControlExplorer.Printing
 {
     public partial class MSPrintDocument : C1DemoForm
     {
-        StreamReader streamToPrint;
-        Font printFont;
+        /// <summary>
+        /// Allows to select whether to use C1PrintDocument or System.Drawing.Printing.PrintDocument to show file content.
+        /// </summary>
+        public enum PrintDocType
+        {
+            MSPrintDocument,
+            C1PrintDocument
+        }
+
+
+        StreamReader _streamToPrint;
+        Font _printFont = new Font("Arial", 10);
+        string _fileName = AppDomain.CurrentDomain.BaseDirectory + "Printing\\Files\\quickstart.txt";
+        private PrintDocType _printDocumentType = PrintDocType.C1PrintDocument;
+
         public MSPrintDocument()
         {
+            AddProperty("PrintDocumentType", this);
+
             InitializeComponent();
 
+            // hide default OpenFile button and use custom one to open file with other parameters
             c1PrintPreviewControl1.ToolBars.File.Open.Visible = false;
-
             var fileOpen = new ToolStripButton("Open", global::ControlExplorer.Properties.Resources.FolderOpen, Open_Click);
             c1PrintPreviewControl1.ToolBars.File.ToolStrip.Items.Insert(0, fileOpen);
 
+            ShowC1Doc();
+        }
+
+        public PrintDocType PrintDocumentType
+        {
+            get => _printDocumentType;
+            set
+            {
+                if (_printDocumentType != value)
+                {
+                    _printDocumentType = value;
+                    if (_printDocumentType == PrintDocType.MSPrintDocument)
+                    {
+                        ShowMSDoc();
+                    }
+                    else
+                    {
+                        ShowC1Doc();
+                    }
+                }
+            }
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "txt files (*.txt)|*.txt";
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    _fileName = openFileDialog.FileName;
+                    if (PrintDocumentType == PrintDocType.MSPrintDocument)
+                    {
+                        ShowMSDoc();
+                    }
+                    else
+                    {
+                        ShowC1Doc();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Show document via C1PrintDocument - put all the text inside RenderText and let C1PrintDocument to handle the paging, word wrapping, etc..
+        /// </summary>
+        private void ShowC1Doc()
+        {
+            string content = System.IO.File.ReadAllText(_fileName, Encoding.Default);
+            var doc = new C1.C1Preview.C1PrintDocument();
+            doc.Body.Children.Add(
+                new C1.C1Preview.RenderText(doc, content));
+            doc.Generate();
+            this.c1PrintPreviewControl1.Document = doc;
+        }
+
+        /// <summary>
+        /// Show document via MS's PrintDocument - generate page content from text lines
+        /// </summary>
+        private void ShowMSDoc()
+        {
             try
             {
-                streamToPrint = new StreamReader(File.Open(AppDomain.CurrentDomain.BaseDirectory + "Printing\\Files\\quickstart.txt", FileMode.Open));
+                _streamToPrint = new StreamReader(File.Open(_fileName, FileMode.Open), Encoding.Default);
                 System.Drawing.Printing.PrintDocument pd = new System.Drawing.Printing.PrintDocument();
                 try
                 {
-                    printFont = new Font("Arial", 10);
                     pd.PrintPage += new System.Drawing.Printing.PrintPageEventHandler
                        (this.pd_PrintPage);
 
@@ -38,7 +115,7 @@ namespace ControlExplorer.Printing
                 }
                 finally
                 {
-                    streamToPrint.Close();
+                    _streamToPrint.Close();
                     pd.PrintPage -= new System.Drawing.Printing.PrintPageEventHandler
                       (this.pd_PrintPage);
                 }
@@ -46,39 +123,6 @@ namespace ControlExplorer.Printing
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
-            }
-
-        }
-
-        private void Open_Click(object sender, EventArgs e)
-        {
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
-            {
-                openFileDialog.InitialDirectory = "c:\\";
-                openFileDialog.Filter = "txt files (*.txt)|*.txt";
-                openFileDialog.FilterIndex = 2;
-                openFileDialog.RestoreDirectory = true;
-
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    //Read the contents of the file into a stream
-                    var fileStream = openFileDialog.OpenFile();
-                    streamToPrint = new StreamReader(fileStream, Encoding.Default);
-                    System.Drawing.Printing.PrintDocument pd = new System.Drawing.Printing.PrintDocument();
-                    try
-                    {
-                        pd.PrintPage += new System.Drawing.Printing.PrintPageEventHandler
-                           (this.pd_PrintPage);
-
-                        this.c1PrintPreviewControl1.Document = pd;
-                    }
-                    finally
-                    {
-                        streamToPrint.Close();
-                        pd.PrintPage -= new System.Drawing.Printing.PrintPageEventHandler
-                          (this.pd_PrintPage);
-                    }
-                }
             }
         }
 
@@ -94,15 +138,15 @@ namespace ControlExplorer.Printing
 
             // Calculate the number of lines per page.
             linesPerPage = ev.MarginBounds.Height /
-               printFont.GetHeight(ev.Graphics);
+               _printFont.GetHeight(ev.Graphics);
 
             // Print each line of the file.
             while (count < linesPerPage &&
-               ((line = streamToPrint.ReadLine()) != null))
+               ((line = _streamToPrint.ReadLine()) != null))
             {
                 yPos = topMargin + (count *
-                   printFont.GetHeight(ev.Graphics));
-                ev.Graphics.DrawString(line, printFont, Brushes.Black,
+                   _printFont.GetHeight(ev.Graphics));
+                ev.Graphics.DrawString(line, _printFont, Brushes.Black,
                    leftMargin, yPos, new StringFormat());
                 count++;
             }
