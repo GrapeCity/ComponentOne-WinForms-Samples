5c82d880f3dfb1a0b39ae90d82d7c6aef0fd6f00
alex.trefilov <alex.trefilov@grapecity.com>
2021-10-13 12:54:42 +0200

    LegendGroups demo added  to FlexPivotExplorer

--- Core/FlexPivot/CS/FlexPivotExplorer/Samples/HierarchicalAxes.cs
+++ Core/FlexPivot/CS/FlexPivotExplorer/Samples/HierarchicalAxes.cs
@@ -25,7 +25,7 @@ namespace FlexPivotExplorer.Samples
         {
             var panel = new C1FlexPivotPanel() { Dock = DockStyle.Fill };
             chart = new FlexPivotChart() { Dock = DockStyle.Fill };
-            panel.DataSource = SampleData.CreateDataTable(1000);
+            panel.DataSource = DataSource.CreateSampleDataTable(1000);
 
             panel.PivotEngine.RowFields.Add("Region", "Country", "Product");
             panel.PivotEngine.ValueFields.Add("Sales");
@@ -41,51 +41,6 @@ namespace FlexPivotExplorer.Samples
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             chart.ShowHierarchicalAxes = checkBox1.Checked;
-        }
-    }
-
-    class SampleData
-    {
-        static Random rnd = new Random();
-
-        public static DataTable CreateDataTable(int nrows)
-        {
-            var dt = new DataTable();
-            dt.Columns.Add("ID", typeof(int));
-            dt.Columns.Add("Product", typeof(string));
-            dt.Columns.Add("Region", typeof(string));
-            dt.Columns.Add("Country", typeof(string));
-            dt.Columns.Add("Sales", typeof(int));
-            dt.Columns.Add("Downloads", typeof(int));
-
-            var countries = "US,Germany,UK,Japan,China,Italy,Spain".Split(',');
-            var products = "Wijmo,Olap".Split(',');
-
-            for (var i = 0; i < nrows; i++)
-            {
-                var country = countries[rnd.Next(countries.Length)];
-                dt.Rows.Add(new object[] {
-                        rnd.Next(),
-                        products[rnd.Next(products.Length)],
-                        CountryToRegion(country),
-                        country,
-                        rnd.Next(10000),
-                        rnd.Next(10000)
-                    }
-                );
-            }
-
-            return dt;
-        }
-
-        static string CountryToRegion(string country)
-        {
-            if (country == "US")
-                return "America";
-            else if (country == "Japan" || country == "China")
-                return "Asia";
-            else
-                return "Europe";
         }
     }
 }
