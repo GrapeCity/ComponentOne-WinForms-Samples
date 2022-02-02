534151faf40a3f58ffc0ffbf67a3415649545484
Vyacheslav Batushin <vyacheslav.batushin@grapecity.com>
2022-02-02 11:55:08 +0300

    Added DashboardLayout.6 samples

---
+++ Next/DashboardLayout/CS/DashboardLayoutDemo/DemoControls/EventArgs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using C1.Win.Layout;
+
+namespace DashboardDemo.Controls
+{
+    public class FilePathEventArgs : EventArgs
+    {
+        public FilePathEventArgs(string path) : base()
+        {
+            FilePath = path;
+        }
+
+        public string FilePath { get; private set; }
+    }
+
+    public class LayoutTypeEventArgs : EventArgs
+    {
+        public LayoutTypeEventArgs(LayoutType type) : base()
+        {
+            Type = type;
+        }
+
+        public LayoutType Type { get; private set; }
+    }
+    public class ToolIconAppearanceEventArgs : EventArgs
+    {
+        public ToolIconAppearanceEventArgs(ToolIconAppearance tia) : base()
+        {
+            ToolIconAppearance = tia;
+        }
+
+        public ToolIconAppearance ToolIconAppearance { get; private set; }
+    }
+}
