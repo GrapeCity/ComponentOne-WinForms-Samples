198ac444a4b34a7f00b233ad8b99957321e9c74c
KonstantinPletnev <konstantin.pletnev@grapecity.com>
2022-02-08 12:07:10 +0300

    C1WIN-26715: added CS XmlImportExport sample for 4.5.2. Updated CS template for 4.5.2 samples.

--- NetFramework/TreeView/CS/XmlImportExport/MainForm.Designer.cs
+++ NetFramework/TreeView/CS/XmlImportExport/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace RibbonAndTheme
+﻿namespace XmlImportExport
 {
     partial class MainForm
     {
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            C1.Win.TreeView.C1TreeColumn c1TreeColumn3 = new C1.Win.TreeView.C1TreeColumn();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.c1Ribbon1 = new C1.Win.Ribbon.C1Ribbon();
             this.ribbonApplicationMenu1 = new C1.Win.Ribbon.RibbonApplicationMenu();
@@ -35,10 +36,17 @@
             this.ribbonConfigToolBar1 = new C1.Win.Ribbon.RibbonConfigToolBar();
             this.ribbonQat1 = new C1.Win.Ribbon.RibbonQat();
             this.ribbonTopToolBar1 = new C1.Win.Ribbon.RibbonTopToolBar();
-            this.c1ThemeController1 = new C1.Win.C1Themes.C1ThemeController();
             this.cmbTheme = new C1.Win.Ribbon.RibbonComboBox();
+            this.c1ThemeController1 = new C1.Win.C1Themes.C1ThemeController();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnExport = new C1.Win.C1Input.C1Button();
+            this.btnImport = new C1.Win.C1Input.C1Button();
+            this.c1TreeView1 = new C1.Win.TreeView.C1TreeView();
             ((System.ComponentModel.ISupportInitialize)(this.c1Ribbon1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.c1ThemeController1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnExport)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnImport)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1TreeView1)).BeginInit();
             this.SuspendLayout();
             // 
             // c1Ribbon1
@@ -84,11 +92,65 @@
             this.cmbTheme.Name = "cmbTheme";
             this.cmbTheme.SelectedIndexChanged += new System.EventHandler(this.cmbTheme_SelectedIndexChanged);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.btnExport, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnImport, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.c1TreeView1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(834, 492);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.c1ThemeController1.SetTheme(this.tableLayoutPanel1, "(default)");
+            // 
+            // btnExport
+            // 
+            this.btnExport.AutoSize = true;
+            this.btnExport.Location = new System.Drawing.Point(420, 3);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(85, 25);
+            this.btnExport.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Export to XML";
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.AutoSize = true;
+            this.btnImport.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnImport.Location = new System.Drawing.Point(312, 3);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(102, 25);
+            this.btnImport.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import from XML";
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // c1TreeView1
+            // 
+            c1TreeColumn3.HeaderText = "Column1";
+            c1TreeColumn3.Name = "Column1";
+            this.c1TreeView1.Columns.Add(c1TreeColumn3);
+            this.tableLayoutPanel1.SetColumnSpan(this.c1TreeView1, 2);
+            this.c1TreeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.c1TreeView1.Location = new System.Drawing.Point(3, 34);
+            this.c1TreeView1.Name = "c1TreeView1";
+            this.c1TreeView1.Size = new System.Drawing.Size(828, 455);
+            this.c1TreeView1.TabIndex = 2;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.c1Ribbon1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
@@ -97,6 +159,9 @@
             this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.c1Ribbon1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.c1ThemeController1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnExport)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnImport)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1TreeView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,6 +177,10 @@
         private C1.Win.Ribbon.RibbonTopToolBar ribbonTopToolBar1;
         private C1.Win.C1Themes.C1ThemeController c1ThemeController1;
         private C1.Win.Ribbon.RibbonComboBox cmbTheme;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private C1.Win.C1Input.C1Button btnImport;
+        private C1.Win.C1Input.C1Button btnExport;
+        private C1.Win.TreeView.C1TreeView c1TreeView1;
     }
 }
 
---
+++ NetFramework/TreeView/CS/XmlImportExport/README.md
@@ -0,0 +1,6 @@
+## XmlImportExport
+#### [Download as zip](https://grapecity.github.io/DownGit/#/home?url=https://github.com/GrapeCity/ComponentOne-WinForms-Samples/tree/master/NetFramework\TreeView\CS\XmlImportExport)
+____
+#### Import & Export to Xml.
+____
+This sample shows how C1TreeView can import simple tree from xml and export tree to xml.
