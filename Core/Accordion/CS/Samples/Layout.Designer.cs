eb39234b3f16a02152e71537b3386a78537bbc9c
Nikita Parkhomenko <nikita.parkhomenko@grapecity.com>
2022-06-09 07:52:37 +0000

    Merged in feature/C1WIN-27076-accordion-samples-net6 (pull request #128)
    Feature/C1WIN-27076 accordion samples net6
    
    * C1WIN-27076 added accordion overview sample.
    
    * C1WIN-27076 updated overview sample
    
    * C1WIN-27076 updated Accordion explorer.
    
    * C1WIN-27076 fixed issues in the sample.
    
    * C1WIN-27076 add accordion to ControloExplorer.
    
    * C1WIN-27076 fixed text
    
    * C1WIN-27076 fixed pkg, add icons to Layout sample
    
    * C1WIN-27076  updated Accordion icon.
    
    
    Approved-by: Denis Zmitrichenko
    Approved-by: Maria Korchemkina

---
+++ Core/Accordion/CS/Samples/Layout.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace AccordionExplorer.Samples
+{
+    partial class Layout
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            C1.Win.Accordion.C1AccordionPage c1AccordionPage1 = new C1.Win.Accordion.C1AccordionPage();
+            C1.Framework.C1BitmapIcon c1BitmapIcon1 = new C1.Framework.C1BitmapIcon();
+            C1.Win.Accordion.C1AccordionPage c1AccordionPage2 = new C1.Win.Accordion.C1AccordionPage();
+            C1.Framework.C1BitmapIcon c1BitmapIcon2 = new C1.Framework.C1BitmapIcon();
+            C1.Win.Accordion.C1AccordionPage c1AccordionPage3 = new C1.Win.Accordion.C1AccordionPage();
+            C1.Framework.C1BitmapIcon c1BitmapIcon3 = new C1.Framework.C1BitmapIcon();
+            C1.Win.Accordion.C1AccordionPage c1AccordionPage4 = new C1.Win.Accordion.C1AccordionPage();
+            C1.Framework.C1BitmapIcon c1BitmapIcon4 = new C1.Framework.C1BitmapIcon();
+            C1.Win.Accordion.C1AccordionPage c1AccordionPage5 = new C1.Win.Accordion.C1AccordionPage();
+            C1.Framework.C1BitmapIcon c1BitmapIcon5 = new C1.Framework.C1BitmapIcon();
+            C1.Win.Accordion.C1AccordionPage c1AccordionPage6 = new C1.Win.Accordion.C1AccordionPage();
+            C1.Win.Accordion.C1AccordionPage c1AccordionPage7 = new C1.Win.Accordion.C1AccordionPage();
+            C1.Win.Accordion.C1AccordionPage c1AccordionPage8 = new C1.Win.Accordion.C1AccordionPage();
+            C1.Win.Accordion.C1AccordionPage c1AccordionPage9 = new C1.Win.Accordion.C1AccordionPage();
+            C1.Win.Accordion.C1AccordionPage c1AccordionPage10 = new C1.Win.Accordion.C1AccordionPage();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Layout));
+            this.c1Accordion1 = new C1.Win.Accordion.C1Accordion();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.expandeAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.collapseAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripDropDownButton2 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripDropDownButton3 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.contextMenuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // c1Accordion1
+            // 
+            this.c1Accordion1.AllowCollapseAll = true;
+            this.c1Accordion1.AllowExpandMany = true;
+            this.c1Accordion1.ContextMenuStrip = this.contextMenuStrip1;
+            this.c1Accordion1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.c1Accordion1.Location = new System.Drawing.Point(0, 34);
+            this.c1Accordion1.Name = "c1Accordion1";
+            c1AccordionPage1.HeaderText = "Page 1";
+            c1BitmapIcon1.Size = new System.Drawing.Size(16, 16);
+            c1BitmapIcon1.Source = global::AccordionExplorer.Properties.Resources.Calendar;
+            c1BitmapIcon1.Stretch = C1.Framework.Stretch.Uniform;
+            c1AccordionPage1.Icon = c1BitmapIcon1;
+            c1AccordionPage1.IsExpanded = true;
+            c1AccordionPage1.Name = "Page 1";
+            c1AccordionPage2.HeaderText = "Page 2";
+            c1BitmapIcon2.Size = new System.Drawing.Size(16, 16);
+            c1BitmapIcon2.Source = global::AccordionExplorer.Properties.Resources.Mail;
+            c1BitmapIcon2.Stretch = C1.Framework.Stretch.Uniform;
+            c1AccordionPage2.Icon = c1BitmapIcon2;
+            c1AccordionPage2.Name = "Page 2";
+            c1AccordionPage3.HeaderText = "Page 3";
+            c1BitmapIcon3.Size = new System.Drawing.Size(16, 16);
+            c1BitmapIcon3.Source = global::AccordionExplorer.Properties.Resources.Mail;
+            c1BitmapIcon3.Stretch = C1.Framework.Stretch.Uniform;
+            c1AccordionPage3.Icon = c1BitmapIcon3;
+            c1AccordionPage3.Name = "Page 3";
+            c1AccordionPage4.HeaderText = "Page 4";
+            c1BitmapIcon4.Size = new System.Drawing.Size(16, 16);
+            c1BitmapIcon4.Source = global::AccordionExplorer.Properties.Resources.Calendar;
+            c1BitmapIcon4.Stretch = C1.Framework.Stretch.Uniform;
+            c1AccordionPage4.Icon = c1BitmapIcon4;
+            c1AccordionPage4.Name = "Page 4";
+            c1AccordionPage5.HeaderText = "Page 5";
+            c1BitmapIcon5.Size = new System.Drawing.Size(16, 16);
+            c1BitmapIcon5.Source = global::AccordionExplorer.Properties.Resources.Calendar;
+            c1BitmapIcon5.Stretch = C1.Framework.Stretch.Uniform;
+            c1AccordionPage5.Icon = c1BitmapIcon5;
+            c1AccordionPage5.Name = "Page 5";
+            c1AccordionPage6.HeaderText = "Page 6";
+            c1AccordionPage6.Name = "Page 6";
+            c1AccordionPage7.HeaderText = "Page 7";
+            c1AccordionPage7.Name = "Page 7";
+            c1AccordionPage8.HeaderText = "Page 8";
+            c1AccordionPage8.Name = "Page 8";
+            c1AccordionPage9.HeaderText = "Page 9";
+            c1AccordionPage9.Name = "Page 9";
+            c1AccordionPage10.HeaderText = "Page 10";
+            c1AccordionPage10.Name = "Page 10";
+            this.c1Accordion1.Pages.Add(c1AccordionPage1);
+            this.c1Accordion1.Pages.Add(c1AccordionPage2);
+            this.c1Accordion1.Pages.Add(c1AccordionPage3);
+            this.c1Accordion1.Pages.Add(c1AccordionPage4);
+            this.c1Accordion1.Pages.Add(c1AccordionPage5);
+            this.c1Accordion1.Pages.Add(c1AccordionPage6);
+            this.c1Accordion1.Pages.Add(c1AccordionPage7);
+            this.c1Accordion1.Pages.Add(c1AccordionPage8);
+            this.c1Accordion1.Pages.Add(c1AccordionPage9);
+            this.c1Accordion1.Pages.Add(c1AccordionPage10);
+            this.c1Accordion1.Size = new System.Drawing.Size(816, 535);
+            this.c1Accordion1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.expandeAllToolStripMenuItem,
+            this.collapseAllToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(177, 68);
+            // 
+            // expandeAllToolStripMenuItem
+            // 
+            this.expandeAllToolStripMenuItem.Name = "expandeAllToolStripMenuItem";
+            this.expandeAllToolStripMenuItem.Size = new System.Drawing.Size(176, 32);
+            this.expandeAllToolStripMenuItem.Text = "Expande All";
+            this.expandeAllToolStripMenuItem.Click += new System.EventHandler(this.expandeAllToolStripMenuItem_Click);
+            // 
+            // collapseAllToolStripMenuItem
+            // 
+            this.collapseAllToolStripMenuItem.Name = "collapseAllToolStripMenuItem";
+            this.collapseAllToolStripMenuItem.Size = new System.Drawing.Size(176, 32);
+            this.collapseAllToolStripMenuItem.Text = "Collapse All";
+            this.collapseAllToolStripMenuItem.Click += new System.EventHandler(this.collapseAllToolStripMenuItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.toolStripSeparator1,
+            this.toolStripDropDownButton2,
+            this.toolStripSeparator2,
+            this.toolStripDropDownButton3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(816, 34);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(159, 29);
+            this.toolStripDropDownButton1.Text = "ExpandDirection";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 34);
+            // 
+            // toolStripDropDownButton2
+            // 
+            this.toolStripDropDownButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton2.Image")));
+            this.toolStripDropDownButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton2.Name = "toolStripDropDownButton2";
+            this.toolStripDropDownButton2.Size = new System.Drawing.Size(119, 29);
+            this.toolStripDropDownButton2.Text = "RightToLeft";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 34);
+            // 
+            // toolStripDropDownButton3
+            // 
+            this.toolStripDropDownButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton3.Image")));
+            this.toolStripDropDownButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton3.Name = "toolStripDropDownButton3";
+            this.toolStripDropDownButton3.Size = new System.Drawing.Size(71, 29);
+            this.toolStripDropDownButton3.Text = "Dock";
+            // 
+            // Layout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.c1Accordion1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Layout";
+            this.Size = new System.Drawing.Size(816, 569);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private C1.Win.Accordion.C1Accordion c1Accordion1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem expandeAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem collapseAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton3;
+    }
+}
