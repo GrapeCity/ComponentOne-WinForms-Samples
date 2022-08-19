da9d12a17e29c17c62e42ba2f28bb64c9555d4d4
Konstantin Pletnev <konstantin.pletnev@grapecity.com>
2022-08-19 05:54:22 +0000

    Merged in feature/C1WIN-25258-create-multiselectexplorer-w (pull request #140)
    C1WIN-25258: Added MultiSelectExplorer with several samples.
    
    * C1WIN-25258: Added MultiSelectExplorer with several samples.
    
    
    Approved-by: Maria Korchemkina

---
+++ Core/MultiSelect/CS/MultiSelectExplorer/Samples/MailMessageForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace MultiSelectExplorer.Samples
+{
+    public partial class MailMessageForm : UserControl
+    {
+        public MailMessageForm()
+        {
+            InitializeComponent();
+            SetupDataSource();
+            Load += NewMessageForm_Load;
+        }
+
+        private void NewMessageForm_Load(object sender, EventArgs e)
+        {
+            ccMultiSelect.BindingInfo.DataSource = toMultiSelect.BindingInfo.DataSource = bindingSource1;
+            ccMultiSelect.BindingInfo.DisplayMemberPath = toMultiSelect.BindingInfo.DisplayMemberPath = "email";                        
+        }
+
+        void SetupDataSource()
+        {
+            string fileName = "us-500.csv";
+
+            string sql = @"SELECT * FROM [" + fileName + "]";
+            DataTable dataTable = new DataTable();
+
+            using (OleDbConnection connection = new OleDbConnection(
+                      @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + System.IO.Directory.GetCurrentDirectory() + "//Resources" +
+                      ";Extended Properties=\"Text;HDR=Yes\""))
+            using (OleDbCommand command = new OleDbCommand(sql, connection))
+            using (OleDbDataAdapter adapter = new OleDbDataAdapter(command))
+            {
+                dataTable.Locale = CultureInfo.CurrentCulture;
+                adapter.Fill(dataTable);
+            }
+
+            // add calculated column to keep full name we need to display in UI
+            dataTable.Columns.Add("full_name", typeof(string));
+            foreach (DataRow row in dataTable.Rows)
+            {
+                row["full_name"] = row["first_name"] + " " + row["last_name"];
+            }
+            this.bindingSource1.DataSource = dataTable;
+        }
+    }
+}
