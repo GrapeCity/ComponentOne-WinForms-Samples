c365305c049ec6056b3abb6187f7db04faad481d
Konstantin Pletnev <konstantin.pletnev@grapecity.com>
2022-10-18 16:55:16 +0000

    Merged in feature/C1WIN-28217-sample-for-c1win-27279-featu (pull request #149)
    Feature/C1WIN-28217 sample for c1win 27279 featu
    
    * C1WIN-28217: Added Input\VB\Placeholder sample.
    
    * C1WIN-28217: Added Input\CS\Placeholder sample.
    
    
    Approved-by: Denis Zmitrichenko

---
+++ NetFramework/Input/VB/Placeholder/Form1.Designer.vb
@@ -0,0 +1,334 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.c1Label5 = New C1.Win.C1Input.C1Label()
+        Me.c1Label4 = New C1.Win.C1Input.C1Label()
+        Me.c1Label3 = New C1.Win.C1Input.C1Label()
+        Me.c1Label2 = New C1.Win.C1Input.C1Label()
+        Me.c1Label1 = New C1.Win.C1Input.C1Label()
+        Me.c1DropDownControl1 = New C1.Win.C1Input.C1DropDownControl()
+        Me.c1NumericEdit1 = New C1.Win.C1Input.C1NumericEdit()
+        Me.c1DateEdit1 = New C1.Win.C1Input.C1DateEdit()
+        Me.c1ComboBox1 = New C1.Win.C1Input.C1ComboBox()
+        Me.c1TextBox1 = New C1.Win.C1Input.C1TextBox()
+        Me.c1Ribbon1 = New C1.Win.Ribbon.C1Ribbon()
+        Me.ribbonApplicationMenu1 = New C1.Win.Ribbon.RibbonApplicationMenu()
+        Me.ribbonBottomToolBar1 = New C1.Win.Ribbon.RibbonBottomToolBar()
+        Me.ribbonConfigToolBar1 = New C1.Win.Ribbon.RibbonConfigToolBar()
+        Me.ribbonQat1 = New C1.Win.Ribbon.RibbonQat()
+        Me.ribbonTab1 = New C1.Win.Ribbon.RibbonTab()
+        Me.ribbonGroup1 = New C1.Win.Ribbon.RibbonGroup()
+        Me.ribbonTopToolBar1 = New C1.Win.Ribbon.RibbonTopToolBar()
+        Me.cmbTheme = New C1.Win.Ribbon.RibbonComboBox()
+        Me.C1ThemeController1 = New C1.Win.C1Themes.C1ThemeController()
+        CType(Me.c1Label5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.c1Label4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.c1Label3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.c1Label2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.c1Label1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.c1DropDownControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.c1NumericEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.c1DateEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.c1ComboBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.c1TextBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.c1Ribbon1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.C1ThemeController1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'c1Label5
+        '
+        Me.c1Label5.AutoSize = True
+        Me.c1Label5.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.c1Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.c1Label5.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.c1Label5.Location = New System.Drawing.Point(8, 265)
+        Me.c1Label5.Name = "c1Label5"
+        Me.c1Label5.Size = New System.Drawing.Size(104, 13)
+        Me.c1Label5.TabIndex = 21
+        Me.c1Label5.Tag = Nothing
+        Me.c1Label5.Text = "C1DropDownControl"
+        Me.c1Label5.TextDetached = True
+        Me.C1ThemeController1.SetTheme(Me.c1Label5, "(default)")
+        '
+        'c1Label4
+        '
+        Me.c1Label4.AutoSize = True
+        Me.c1Label4.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.c1Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.c1Label4.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.c1Label4.Location = New System.Drawing.Point(35, 219)
+        Me.c1Label4.Name = "c1Label4"
+        Me.c1Label4.Size = New System.Drawing.Size(77, 13)
+        Me.c1Label4.TabIndex = 20
+        Me.c1Label4.Tag = Nothing
+        Me.c1Label4.Text = "C1NumericEdit"
+        Me.c1Label4.TextDetached = True
+        Me.C1ThemeController1.SetTheme(Me.c1Label4, "(default)")
+        '
+        'c1Label3
+        '
+        Me.c1Label3.AutoSize = True
+        Me.c1Label3.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.c1Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.c1Label3.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.c1Label3.Location = New System.Drawing.Point(51, 174)
+        Me.c1Label3.Name = "c1Label3"
+        Me.c1Label3.Size = New System.Drawing.Size(61, 13)
+        Me.c1Label3.TabIndex = 19
+        Me.c1Label3.Tag = Nothing
+        Me.c1Label3.Text = "C1DateEdit"
+        Me.c1Label3.TextDetached = True
+        Me.C1ThemeController1.SetTheme(Me.c1Label3, "(default)")
+        '
+        'c1Label2
+        '
+        Me.c1Label2.AutoSize = True
+        Me.c1Label2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.c1Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.c1Label2.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.c1Label2.Location = New System.Drawing.Point(41, 128)
+        Me.c1Label2.Name = "c1Label2"
+        Me.c1Label2.Size = New System.Drawing.Size(71, 13)
+        Me.c1Label2.TabIndex = 18
+        Me.c1Label2.Tag = Nothing
+        Me.c1Label2.Text = "C1ComboBox"
+        Me.c1Label2.TextDetached = True
+        Me.C1ThemeController1.SetTheme(Me.c1Label2, "(default)")
+        '
+        'c1Label1
+        '
+        Me.c1Label1.AutoSize = True
+        Me.c1Label1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.c1Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.c1Label1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.c1Label1.Location = New System.Drawing.Point(53, 86)
+        Me.c1Label1.Name = "c1Label1"
+        Me.c1Label1.Size = New System.Drawing.Size(59, 13)
+        Me.c1Label1.TabIndex = 17
+        Me.c1Label1.Tag = Nothing
+        Me.c1Label1.Text = "C1TextBox"
+        Me.c1Label1.TextDetached = True
+        Me.C1ThemeController1.SetTheme(Me.c1Label1, "(default)")
+        '
+        'c1DropDownControl1
+        '
+        Me.c1DropDownControl1.BackColor = System.Drawing.Color.FromArgb(CType(CType(234, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.c1DropDownControl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.c1DropDownControl1.GapHeight = 0
+        Me.c1DropDownControl1.ImagePadding = New System.Windows.Forms.Padding(0)
+        Me.c1DropDownControl1.Location = New System.Drawing.Point(118, 262)
+        Me.c1DropDownControl1.Name = "c1DropDownControl1"
+        Me.c1DropDownControl1.Placeholder = "C1DropDownControl placeholder"
+        Me.c1DropDownControl1.Size = New System.Drawing.Size(200, 18)
+        Me.c1DropDownControl1.TabIndex = 16
+        Me.c1DropDownControl1.Tag = Nothing
+        Me.C1ThemeController1.SetTheme(Me.c1DropDownControl1, "(default)")
+        Me.c1DropDownControl1.VisualStyleBaseStyle = C1.Win.C1Input.VisualStyle.Office2010Blue
+        '
+        'c1NumericEdit1
+        '
+        Me.c1NumericEdit1.BackColor = System.Drawing.Color.FromArgb(CType(CType(234, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.c1NumericEdit1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        '
+        '
+        '
+        Me.c1NumericEdit1.Calculator.BackColor = System.Drawing.Color.FromArgb(CType(CType(227, Byte), Integer), CType(CType(239, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.c1NumericEdit1.Calculator.VisualStyle = C1.Win.C1Input.VisualStyle.Office2010Blue
+        Me.c1NumericEdit1.Calculator.VisualStyleBaseStyle = C1.Win.C1Input.VisualStyle.Office2010Blue
+        Me.c1NumericEdit1.GapHeight = 0
+        Me.c1NumericEdit1.ImagePadding = New System.Windows.Forms.Padding(0)
+        Me.c1NumericEdit1.Location = New System.Drawing.Point(118, 216)
+        Me.c1NumericEdit1.Name = "c1NumericEdit1"
+        Me.c1NumericEdit1.Placeholder = "C1NumericEdit placeholder"
+        Me.c1NumericEdit1.Size = New System.Drawing.Size(200, 18)
+        Me.c1NumericEdit1.TabIndex = 15
+        Me.c1NumericEdit1.Tag = Nothing
+        Me.C1ThemeController1.SetTheme(Me.c1NumericEdit1, "(default)")
+        Me.c1NumericEdit1.VisualStyleBaseStyle = C1.Win.C1Input.VisualStyle.Office2010Blue
+        '
+        'c1DateEdit1
+        '
+        Me.c1DateEdit1.BackColor = System.Drawing.Color.FromArgb(CType(CType(234, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.c1DateEdit1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        '
+        '
+        '
+        Me.c1DateEdit1.Calendar.Font = New System.Drawing.Font("Tahoma", 8.0!)
+        Me.c1DateEdit1.Calendar.VisualStyle = C1.Win.C1Input.VisualStyle.System
+        Me.c1DateEdit1.Calendar.VisualStyleBaseStyle = C1.Win.C1Input.VisualStyle.Office2010Blue
+        Me.c1DateEdit1.GapHeight = 0
+        Me.c1DateEdit1.ImagePadding = New System.Windows.Forms.Padding(0)
+        Me.c1DateEdit1.Location = New System.Drawing.Point(118, 171)
+        Me.c1DateEdit1.Name = "c1DateEdit1"
+        Me.c1DateEdit1.Placeholder = "C1DateEdit placeholder"
+        Me.c1DateEdit1.Size = New System.Drawing.Size(200, 18)
+        Me.c1DateEdit1.TabIndex = 14
+        Me.c1DateEdit1.Tag = Nothing
+        Me.C1ThemeController1.SetTheme(Me.c1DateEdit1, "(default)")
+        Me.c1DateEdit1.VisualStyleBaseStyle = C1.Win.C1Input.VisualStyle.Office2010Blue
+        '
+        'c1ComboBox1
+        '
+        Me.c1ComboBox1.AllowSpinLoop = False
+        Me.c1ComboBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(234, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.c1ComboBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.c1ComboBox1.GapHeight = 0
+        Me.c1ComboBox1.ImagePadding = New System.Windows.Forms.Padding(0)
+        Me.c1ComboBox1.Items.Add("one")
+        Me.c1ComboBox1.Items.Add("two")
+        Me.c1ComboBox1.Location = New System.Drawing.Point(118, 125)
+        Me.c1ComboBox1.Name = "c1ComboBox1"
+        Me.c1ComboBox1.Placeholder = "C1ComboBox placeholder"
+        Me.c1ComboBox1.Size = New System.Drawing.Size(200, 18)
+        Me.c1ComboBox1.TabIndex = 13
+        Me.c1ComboBox1.Tag = Nothing
+        Me.C1ThemeController1.SetTheme(Me.c1ComboBox1, "(default)")
+        Me.c1ComboBox1.VisualStyleBaseStyle = C1.Win.C1Input.VisualStyle.Office2010Blue
+        '
+        'c1TextBox1
+        '
+        Me.c1TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(234, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.c1TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.c1TextBox1.Location = New System.Drawing.Point(118, 83)
+        Me.c1TextBox1.Name = "c1TextBox1"
+        Me.c1TextBox1.Placeholder = "C1TextBox placeholder"
+        Me.c1TextBox1.Size = New System.Drawing.Size(200, 18)
+        Me.c1TextBox1.TabIndex = 12
+        Me.c1TextBox1.Tag = Nothing
+        Me.C1ThemeController1.SetTheme(Me.c1TextBox1, "(default)")
+        Me.c1TextBox1.VisualStyleBaseStyle = C1.Win.C1Input.VisualStyle.Office2010Blue
+        '
+        'c1Ribbon1
+        '
+        Me.c1Ribbon1.ApplicationMenuHolder = Me.ribbonApplicationMenu1
+        Me.c1Ribbon1.BottomToolBarHolder = Me.ribbonBottomToolBar1
+        Me.c1Ribbon1.ConfigToolBarHolder = Me.ribbonConfigToolBar1
+        Me.c1Ribbon1.HideTabHeaderRow = True
+        Me.c1Ribbon1.Location = New System.Drawing.Point(0, 0)
+        Me.c1Ribbon1.Minimized = True
+        Me.c1Ribbon1.Name = "c1Ribbon1"
+        Me.c1Ribbon1.QatHolder = Me.ribbonQat1
+        Me.c1Ribbon1.Size = New System.Drawing.Size(446, 34)
+        Me.c1Ribbon1.Tabs.Add(Me.ribbonTab1)
+        Me.C1ThemeController1.SetTheme(Me.c1Ribbon1, "(default)")
+        Me.c1Ribbon1.TopToolBarHolder = Me.ribbonTopToolBar1
+        '
+        'ribbonApplicationMenu1
+        '
+        Me.ribbonApplicationMenu1.Name = "ribbonApplicationMenu1"
+        Me.ribbonApplicationMenu1.Visible = False
+        '
+        'ribbonBottomToolBar1
+        '
+        Me.ribbonBottomToolBar1.Name = "ribbonBottomToolBar1"
+        '
+        'ribbonConfigToolBar1
+        '
+        Me.ribbonConfigToolBar1.Name = "ribbonConfigToolBar1"
+        '
+        'ribbonQat1
+        '
+        Me.ribbonQat1.Name = "ribbonQat1"
+        '
+        'ribbonTab1
+        '
+        Me.ribbonTab1.Groups.Add(Me.ribbonGroup1)
+        Me.ribbonTab1.Name = "ribbonTab1"
+        Me.ribbonTab1.Text = "Tab"
+        '
+        'ribbonGroup1
+        '
+        Me.ribbonGroup1.Name = "ribbonGroup1"
+        Me.ribbonGroup1.Text = "Group"
+        '
+        'ribbonTopToolBar1
+        '
+        Me.ribbonTopToolBar1.Items.Add(Me.cmbTheme)
+        Me.ribbonTopToolBar1.Name = "ribbonTopToolBar1"
+        '
+        'cmbTheme
+        '
+        Me.cmbTheme.Label = "Theme"
+        Me.cmbTheme.Name = "cmbTheme"
+        Me.cmbTheme.TextAreaWidth = 120
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(446, 305)
+        Me.Controls.Add(Me.c1Label5)
+        Me.Controls.Add(Me.c1Label4)
+        Me.Controls.Add(Me.c1Label3)
+        Me.Controls.Add(Me.c1Label2)
+        Me.Controls.Add(Me.c1Label1)
+        Me.Controls.Add(Me.c1DropDownControl1)
+        Me.Controls.Add(Me.c1NumericEdit1)
+        Me.Controls.Add(Me.c1DateEdit1)
+        Me.Controls.Add(Me.c1ComboBox1)
+        Me.Controls.Add(Me.c1TextBox1)
+        Me.Controls.Add(Me.c1Ribbon1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "C1Input Placeholder"
+        Me.C1ThemeController1.SetTheme(Me, "(default)")
+        CType(Me.c1Label5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.c1Label4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.c1Label3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.c1Label2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.c1Label1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.c1DropDownControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.c1NumericEdit1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.c1DateEdit1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.c1ComboBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.c1TextBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.c1Ribbon1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.C1ThemeController1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents c1Label5 As C1.Win.C1Input.C1Label
+    Private WithEvents c1Label4 As C1.Win.C1Input.C1Label
+    Private WithEvents c1Label3 As C1.Win.C1Input.C1Label
+    Private WithEvents c1Label2 As C1.Win.C1Input.C1Label
+    Private WithEvents c1Label1 As C1.Win.C1Input.C1Label
+    Private WithEvents c1DropDownControl1 As C1.Win.C1Input.C1DropDownControl
+    Private WithEvents c1NumericEdit1 As C1.Win.C1Input.C1NumericEdit
+    Private WithEvents c1DateEdit1 As C1.Win.C1Input.C1DateEdit
+    Private WithEvents c1ComboBox1 As C1.Win.C1Input.C1ComboBox
+    Private WithEvents c1TextBox1 As C1.Win.C1Input.C1TextBox
+    Private WithEvents c1Ribbon1 As C1.Win.Ribbon.C1Ribbon
+    Private WithEvents ribbonApplicationMenu1 As C1.Win.Ribbon.RibbonApplicationMenu
+    Private WithEvents ribbonBottomToolBar1 As C1.Win.Ribbon.RibbonBottomToolBar
+    Private WithEvents ribbonConfigToolBar1 As C1.Win.Ribbon.RibbonConfigToolBar
+    Private WithEvents ribbonQat1 As C1.Win.Ribbon.RibbonQat
+    Private WithEvents ribbonTab1 As C1.Win.Ribbon.RibbonTab
+    Private WithEvents ribbonGroup1 As C1.Win.Ribbon.RibbonGroup
+    Private WithEvents ribbonTopToolBar1 As C1.Win.Ribbon.RibbonTopToolBar
+    Private WithEvents cmbTheme As C1.Win.Ribbon.RibbonComboBox
+    Friend WithEvents C1ThemeController1 As C1.Win.C1Themes.C1ThemeController
+End Class
---
+++ NetFramework/Input/VB/Placeholder/readme.txt
@@ -0,0 +1,5 @@
+﻿Placeholder
+-----------------------------
+The textual place holder.
+
+This sample shows how to use the textual place holder, that is displayed by C1Input text editors to prompt the user for information.
