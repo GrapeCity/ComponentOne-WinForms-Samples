587510456dae39803b9c9f4a4ac37e92e0a8cfdd
Konstantin Pletnev <konstantin.pletnev@grapecity.com>
2022-10-18 16:55:22 +0000

    Merged in feature/C1WIN-28218-sample-for-c1win-26798-featu (pull request #151)
    Feature/C1WIN-28218 sample for c1win 26798 featu
    
    * C1WIN-28218: Added Input\CS\SplitButtonItemStyle sample.
    
    * C1WIN-28218: Added Input\VB\SplitButtonItemStylesample.
    
    * C1WIN-28218: fixed AssemblyInfo
    
    
    Approved-by: Nikita Parkhomenko

---
+++ NetFramework/Input/CS/SplitButtonItemStyle/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace SplitButtonItemStyle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.c1SplitButton1 = new C1.Win.C1Input.C1SplitButton();
+            this.c1SplitButton2 = new C1.Win.C1Input.C1SplitButton();
+            this.c1SplitButton3 = new C1.Win.C1Input.C1SplitButton();
+            this.dropDownItem1 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem2 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem3 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem4 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem5 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem6 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem7 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem8 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem9 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem10 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem11 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem12 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem13 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem14 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem15 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem16 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem17 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem18 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem19 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem20 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem21 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem22 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem23 = new C1.Win.C1Input.DropDownItem();
+            this.dropDownItem24 = new C1.Win.C1Input.DropDownItem();
+            ((System.ComponentModel.ISupportInitialize)(this.c1SplitButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1SplitButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1SplitButton3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // c1SplitButton1
+            // 
+            this.c1SplitButton1.Items.Add(this.dropDownItem1);
+            this.c1SplitButton1.Items.Add(this.dropDownItem2);
+            this.c1SplitButton1.Items.Add(this.dropDownItem3);
+            this.c1SplitButton1.Items.Add(this.dropDownItem4);
+            this.c1SplitButton1.ItemStyle.BackColor = System.Drawing.SystemColors.Info;
+            this.c1SplitButton1.ItemStyle.DisabledForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.c1SplitButton1.ItemStyle.ImageBarBackColor = System.Drawing.SystemColors.Info;
+            this.c1SplitButton1.ItemStyle.ImageBarSeparatorColor = System.Drawing.SystemColors.ActiveCaption;
+            this.c1SplitButton1.Location = new System.Drawing.Point(48, 61);
+            this.c1SplitButton1.Name = "c1SplitButton1";
+            this.c1SplitButton1.Size = new System.Drawing.Size(115, 28);
+            this.c1SplitButton1.TabIndex = 1;
+            this.c1SplitButton1.Text = "c1SplitButton1";
+            this.c1SplitButton1.UseVisualStyleBackColor = true;
+            this.c1SplitButton1.UseVisualStyleForeColor = true;
+            // 
+            // c1SplitButton2
+            // 
+            this.c1SplitButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.c1SplitButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.c1SplitButton2.ForeColor = System.Drawing.SystemColors.Window;
+            this.c1SplitButton2.Items.Add(this.dropDownItem9);
+            this.c1SplitButton2.Items.Add(this.dropDownItem10);
+            this.c1SplitButton2.Items.Add(this.dropDownItem11);
+            this.c1SplitButton2.Items.Add(this.dropDownItem12);
+            this.c1SplitButton2.ItemStyle.ArrowColor = System.Drawing.SystemColors.Window;
+            this.c1SplitButton2.ItemStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.c1SplitButton2.ItemStyle.ForeColor = System.Drawing.SystemColors.Window;
+            this.c1SplitButton2.ItemStyle.HotBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.c1SplitButton2.ItemStyle.HotForeColor = System.Drawing.SystemColors.Window;
+            this.c1SplitButton2.ItemStyle.ImageBarBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.c1SplitButton2.ItemStyle.ImageBarSeparatorColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.c1SplitButton2.Location = new System.Drawing.Point(208, 61);
+            this.c1SplitButton2.Name = "c1SplitButton2";
+            this.c1SplitButton2.Size = new System.Drawing.Size(115, 28);
+            this.c1SplitButton2.TabIndex = 2;
+            this.c1SplitButton2.Text = "c1SplitButton2";
+            this.c1SplitButton2.UseVisualStyleBackColor = false;
+            this.c1SplitButton2.UseVisualStyleForeColor = false;
+            // 
+            // c1SplitButton3
+            // 
+            this.c1SplitButton3.BackColor = System.Drawing.SystemColors.Window;
+            this.c1SplitButton3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.c1SplitButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.c1SplitButton3.Items.Add(this.dropDownItem17);
+            this.c1SplitButton3.Items.Add(this.dropDownItem18);
+            this.c1SplitButton3.Items.Add(this.dropDownItem19);
+            this.c1SplitButton3.Items.Add(this.dropDownItem20);
+            this.c1SplitButton3.ItemStyle.ArrowColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.c1SplitButton3.ItemStyle.DisabledForeColor = System.Drawing.Color.DimGray;
+            this.c1SplitButton3.ItemStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.c1SplitButton3.ItemStyle.HotBackColor = System.Drawing.Color.LightSkyBlue;
+            this.c1SplitButton3.ItemStyle.HotForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.c1SplitButton3.ItemStyle.ImageBarBackColor = System.Drawing.SystemColors.Window;
+            this.c1SplitButton3.Location = new System.Drawing.Point(364, 61);
+            this.c1SplitButton3.Name = "c1SplitButton3";
+            this.c1SplitButton3.Size = new System.Drawing.Size(115, 28);
+            this.c1SplitButton3.TabIndex = 3;
+            this.c1SplitButton3.Text = "c1SplitButton3";
+            this.c1SplitButton3.UseVisualStyleBackColor = false;
+            this.c1SplitButton3.UseVisualStyleForeColor = true;
+            // 
+            // dropDownItem1
+            // 
+            this.dropDownItem1.Enabled = false;
+            this.dropDownItem1.Text = "dropDownItem1";
+            // 
+            // dropDownItem2
+            // 
+            this.dropDownItem2.Items.Add(this.dropDownItem5);
+            this.dropDownItem2.Items.Add(this.dropDownItem6);
+            this.dropDownItem2.Text = "dropDownItem2";
+            // 
+            // dropDownItem3
+            // 
+            this.dropDownItem3.Text = "dropDownItem3";
+            // 
+            // dropDownItem4
+            // 
+            this.dropDownItem4.Items.Add(this.dropDownItem7);
+            this.dropDownItem4.Items.Add(this.dropDownItem8);
+            this.dropDownItem4.Text = "dropDownItem4";
+            // 
+            // dropDownItem5
+            // 
+            this.dropDownItem5.Text = "dropDownItem5";
+            // 
+            // dropDownItem6
+            // 
+            this.dropDownItem6.Text = "dropDownItem6";
+            // 
+            // dropDownItem7
+            // 
+            this.dropDownItem7.Text = "dropDownItem7";
+            // 
+            // dropDownItem8
+            // 
+            this.dropDownItem8.Text = "dropDownItem8";
+            // 
+            // dropDownItem9
+            // 
+            this.dropDownItem9.Items.Add(this.dropDownItem13);
+            this.dropDownItem9.Items.Add(this.dropDownItem14);
+            this.dropDownItem9.Text = "dropDownItem9";
+            // 
+            // dropDownItem10
+            // 
+            this.dropDownItem10.Enabled = false;
+            this.dropDownItem10.Text = "dropDownItem10";
+            // 
+            // dropDownItem11
+            // 
+            this.dropDownItem11.Items.Add(this.dropDownItem15);
+            this.dropDownItem11.Items.Add(this.dropDownItem16);
+            this.dropDownItem11.Text = "dropDownItem11";
+            // 
+            // dropDownItem12
+            // 
+            this.dropDownItem12.Text = "dropDownItem12";
+            // 
+            // dropDownItem13
+            // 
+            this.dropDownItem13.Text = "dropDownItem13";
+            // 
+            // dropDownItem14
+            // 
+            this.dropDownItem14.Text = "dropDownItem14";
+            // 
+            // dropDownItem15
+            // 
+            this.dropDownItem15.Text = "dropDownItem15";
+            // 
+            // dropDownItem16
+            // 
+            this.dropDownItem16.Text = "dropDownItem16";
+            // 
+            // dropDownItem17
+            // 
+            this.dropDownItem17.Items.Add(this.dropDownItem21);
+            this.dropDownItem17.Items.Add(this.dropDownItem22);
+            this.dropDownItem17.Text = "dropDownItem17";
+            // 
+            // dropDownItem18
+            // 
+            this.dropDownItem18.Text = "dropDownItem18";
+            // 
+            // dropDownItem19
+            // 
+            this.dropDownItem19.Enabled = false;
+            this.dropDownItem19.Text = "dropDownItem19";
+            // 
+            // dropDownItem20
+            // 
+            this.dropDownItem20.Items.Add(this.dropDownItem23);
+            this.dropDownItem20.Items.Add(this.dropDownItem24);
+            this.dropDownItem20.Text = "dropDownItem20";
+            // 
+            // dropDownItem21
+            // 
+            this.dropDownItem21.Text = "dropDownItem21";
+            // 
+            // dropDownItem22
+            // 
+            this.dropDownItem22.Text = "dropDownItem22";
+            // 
+            // dropDownItem23
+            // 
+            this.dropDownItem23.Text = "dropDownItem23";
+            // 
+            // dropDownItem24
+            // 
+            this.dropDownItem24.Text = "dropDownItem24";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 172);
+            this.Controls.Add(this.c1SplitButton3);
+            this.Controls.Add(this.c1SplitButton2);
+            this.Controls.Add(this.c1SplitButton1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SplitButton ItemStyle";
+            ((System.ComponentModel.ISupportInitialize)(this.c1SplitButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1SplitButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1SplitButton3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private C1.Win.C1Input.C1SplitButton c1SplitButton1;
+        private C1.Win.C1Input.C1SplitButton c1SplitButton2;
+        private C1.Win.C1Input.C1SplitButton c1SplitButton3;
+        private C1.Win.C1Input.DropDownItem dropDownItem1;
+        private C1.Win.C1Input.DropDownItem dropDownItem2;
+        private C1.Win.C1Input.DropDownItem dropDownItem5;
+        private C1.Win.C1Input.DropDownItem dropDownItem6;
+        private C1.Win.C1Input.DropDownItem dropDownItem3;
+        private C1.Win.C1Input.DropDownItem dropDownItem4;
+        private C1.Win.C1Input.DropDownItem dropDownItem7;
+        private C1.Win.C1Input.DropDownItem dropDownItem8;
+        private C1.Win.C1Input.DropDownItem dropDownItem9;
+        private C1.Win.C1Input.DropDownItem dropDownItem13;
+        private C1.Win.C1Input.DropDownItem dropDownItem14;
+        private C1.Win.C1Input.DropDownItem dropDownItem10;
+        private C1.Win.C1Input.DropDownItem dropDownItem11;
+        private C1.Win.C1Input.DropDownItem dropDownItem15;
+        private C1.Win.C1Input.DropDownItem dropDownItem16;
+        private C1.Win.C1Input.DropDownItem dropDownItem12;
+        private C1.Win.C1Input.DropDownItem dropDownItem17;
+        private C1.Win.C1Input.DropDownItem dropDownItem21;
+        private C1.Win.C1Input.DropDownItem dropDownItem22;
+        private C1.Win.C1Input.DropDownItem dropDownItem18;
+        private C1.Win.C1Input.DropDownItem dropDownItem19;
+        private C1.Win.C1Input.DropDownItem dropDownItem20;
+        private C1.Win.C1Input.DropDownItem dropDownItem23;
+        private C1.Win.C1Input.DropDownItem dropDownItem24;
+    }
+}
+
