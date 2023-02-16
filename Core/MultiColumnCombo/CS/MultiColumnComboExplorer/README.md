f89881f9cb2ed47bca5d55d8415db45cd26c3b55
Konstantin Pletnev <konstantin.pletnev@developertools.com>
2023-02-16 08:18:18 +0000

    Merged in feature/C1WIN-29106-multicolumncombo-review-ce-a (pull request #245)
    Feature/C1WIN-29106 multicolumncombo review ce a
    
    * C1WIN-29106: improved samples:
    1. Added MasterDetail sample in the MultiColumnComboExplorer.
    2. Fixed layout issues in the ControlExplorer.
    3. Chaged newfeatures behavior to default sample.
    4. Added missed samples to the ControlExplorer.
    
    * C1WIN-29106: updated screenshot for MultiColumnComboExplorer.
    
    * C1WIN-29106: categories with new samples are expanded by default. improved data binding sample from InputExplorer.
    
    * C1WIN-29106: improved DataCreating sample.
    
    * C1WIN-29106: restored hidden buttons.
    
    
    Approved-by: Denis Zmitrichenko
    Approved-by: Irina Pykhova

--- Core/MultiColumnCombo/CS/MultiColumnComboExplorer/README.md
+++ Core/MultiColumnCombo/CS/MultiColumnComboExplorer/README.md
@@ -6,7 +6,10 @@ ____
 Includes next samples:
 
 * Overview.
-  This view shows the basic C1MultiColumnCombo features: bound mode, searching, filtering, unbound mode, custom styles.  
+  This view shows the basic C1MultiColumnCombo features: bound mode, searching, filtering, unbound mode, custom styles.
+* Master Detail.
+  This view shows how to use the C1MultiColumnCombo as a hosted control in the C1InputPanel control.
+  C1MultiColumnCombo is used to select the main data item, other panel editors are used to display and edit the main item's details.
 * Custom view.
   This view shows how to implement a custom view for the C1MultiColumnCombo control. The CustomDataGridView uses the DataGridView control as custom dropdown view.
   The CustomTreeView uses the C1TreeView control as custom dropdown view.
---
+++ Core/MultiColumnCombo/CS/MultiColumnComboExplorer/Samples/MasterDetail.cs
@@ -0,0 +1,49 @@
+﻿using C1.Win.InputPanel;
+using MultiColumnComboExplorer.Data;
+using System;
+using System.Windows.Forms;
+
+namespace MultiColumnComboExplorer.Samples
+{
+    public partial class MasterDetail : UserControl
+    {
+        public MasterDetail()
+        {
+            InitializeComponent();
+        }
+
+        private void MasterDetail_Load(object sender, EventArgs e)
+        {
+            var data = new BindingSource(DataSource.GetSalesInfo(), "");
+            CustomizeDetailPanel(data);
+        }
+
+        private void CustomizeDetailPanel(BindingSource source)
+        {            
+            ipDetailed.DataSource = source;
+            // master area
+            ipDetailed.Items.Insert(0, new InputGroupHeader()
+            {
+                Text = "Master"
+            });
+            ipDetailed.Items.Insert(1, new InputLabel()
+            {
+                Text = "Select a product:"
+            });
+            MultiColumnComboHost columnComboHost = new();
+            ipDetailed.Items.Insert(2, columnComboHost);
+            columnComboHost.MultiColumnCombo.DataSource = source;
+            columnComboHost.MultiColumnCombo.DisplayMember = "Product";
+            columnComboHost.MultiColumnCombo.ValueMember = "Id";
+            // detail area
+            ((InputGroupHeader)ipDetailed.Items[3]).Text = "Detail";
+            ipDetailed.Items[4].Visibility = C1.Win.InputPanel.Visibility.Collapsed;
+            ipDetailed.Items[5].Visibility = C1.Win.InputPanel.Visibility.Collapsed;
+            ipDetailed.Items[6].Visibility = C1.Win.InputPanel.Visibility.Collapsed;
+            ipDetailed.Items[7].Visibility = C1.Win.InputPanel.Visibility.Collapsed;
+            ipDetailed.Items[11].Break = C1.Win.InputPanel.BreakType.Column;
+            ((C1.Win.InputPanel.InputNumericBox)ipDetailed.Items[15]).Maximum = 10000;
+            ((C1.Win.InputPanel.InputNumericBox)ipDetailed.Items[17]).Maximum = 10000;
+        }
+    }
+}
