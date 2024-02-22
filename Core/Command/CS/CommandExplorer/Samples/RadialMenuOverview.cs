4a4fbb12019d3b03d4637e615350f507f53726e5
Dominik Galiev <dominik.galiev@developertools.com>
2024-02-22 07:59:01 +0000

    Merged in feature/C1WIN-28661-sample-port-radialmenu-sampl (pull request #354)
    [Sample] [C1RadialMenu] Port RadialMenu samples to .NET Core
    
    * Sample created
    
    * fixed
    
    * control explorer added
    
    
    Approved-by: Maria Korchemkina

---
+++ Core/Command/CS/CommandExplorer/Samples/RadialMenuOverview.cs
@@ -0,0 +1,123 @@
+﻿using C1.Win.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommandExplorer.Samples
+{
+    public partial class RadialMenuOverview : Form
+    {
+        private C1RadialMenu _activeRadialMenu;
+        private int _menuClosedCounter = 1;
+
+        public RadialMenuOverview()
+        {
+            TopLevel = false;
+            TopMost = false;
+            ControlBox = false;
+            FormBorderStyle = FormBorderStyle.None;
+            SizeGripStyle = SizeGripStyle.Hide;
+            InitializeComponent();
+            _activeRadialMenu = this.c1RmC1;
+
+            chkShowOnMouseDown.Checked = true;
+            chkShowExpanded.Checked = true;
+            chkAutoHide.Checked = _activeRadialMenu.AutoHide;
+            chkAnimateMenu.Checked = _activeRadialMenu.UseAnimation;
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            ShowMenu();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (chkShowOnMouseDown.Checked && panel1.ClientRectangle.Contains(new Point(e.X, e.Y)))
+            {
+                _activeRadialMenu.ShowMenu(this, this.PointToScreen(new Point(e.X, e.Y)), chkShowExpanded.Checked);
+            }
+        }
+
+        private void ShowMenu()
+        {
+            _activeRadialMenu.ShowMenu(this, CalcCenter(), chkShowExpanded.Checked);
+        }
+
+        private Point CalcCenter()
+        {
+            int w = tableLayoutPanel1.GetColumnWidths()[0];
+            int h = tableLayoutPanel1.Height;
+            return tableLayoutPanel1.PointToScreen(new Point(w / 2, h / 2));
+        }
+
+        private void RadialMenu_ItemClick(object sender, RadialMenuItemClickEventArgs e)
+        {
+            switch (e.RadialMenuItem.UserData as string)
+            {
+                case "check":
+                    e.RadialMenuItem.Checked = !e.RadialMenuItem.Checked;
+                    e.RadialMenuItem.Text = e.RadialMenuItem.Checked ? "ON" : "OFF";
+                    e.RadialMenuItem.ToolTip = e.RadialMenuItem.Checked ? "Checked item" : "Unchecked item";
+                    e.RadialMenuItem.Image = e.RadialMenuItem.Checked ?
+                        global::CommandExplorer.Properties.Resources.bullet_ball_glass_red :
+                        global::CommandExplorer.Properties.Resources.bullet_ball_glass_grey;
+                    break;
+                case "locale":
+                    RadialMenuItem parent = e.RadialMenuItem.Parent as RadialMenuItem;
+                    if (parent != null)
+                    {
+                        foreach (RadialMenuItem it in parent.Items)
+                            if ((string)it.UserData == "locale")
+                                it.Checked = false;
+                    }
+                    e.RadialMenuItem.Checked = true;
+                    break;
+            }
+        }
+
+        private void UpdateMenuPositionEventHandler(object sender, EventArgs e)
+        {
+            if (_activeRadialMenu.Visible)
+                _activeRadialMenu.CenterOnScreen = CalcCenter();
+        }
+
+        private void rmiHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Help clicked");
+        }
+
+        private void btnShowMenu_Click(object sender, EventArgs e)
+        {
+            ShowMenu();
+        }
+
+        private void btnHideMenu_Click(object sender, EventArgs e)
+        {
+            _activeRadialMenu.HideMenu();
+        }
+
+        private void chkAnimateMenu_CheckedChanged(object sender, EventArgs e)
+        {
+            _activeRadialMenu.UseAnimation = chkAnimateMenu.Checked;
+        }
+
+        private void chkAutoHide_CheckedChanged(object sender, EventArgs e)
+        {
+            _activeRadialMenu.AutoHide = chkAutoHide.Checked;
+        }
+
+        private void RadialMenu_MenuClosed(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine($"Menu closed {_menuClosedCounter}");
+            _menuClosedCounter++;
+        }
+    }
+}
