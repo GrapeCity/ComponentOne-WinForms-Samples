9747a19b487ebb52091197c91e1269732c5b331a
Rei Basha <rei.basha@developertools.com>
2024-04-19 12:07:12 +0000

    Merged in feature/C1WIN-31855-create-a-new-sample-printand (pull request #357)

---
+++ Core/FlexReport/CS/ExportFromFlexReportAndPdf/PasswordForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ExportFromFlexReportAndPdf
+{
+    partial class PasswordForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PasswordForm));
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            tbPassword = new TextBox();
+            label1 = new Label();
+            btnOk = new Button();
+            btnCancel = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(16, 18);
+            pictureBox1.Margin = new Padding(4, 5, 4, 5);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(64, 74);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(88, 18);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(470, 20);
+            label2.TabIndex = 0;
+            label2.Text = "'FileName.png' is protected. Please enter a Document Open Password.";
+            // 
+            // tbPassword
+            // 
+            tbPassword.Location = new Point(208, 55);
+            tbPassword.Margin = new Padding(4, 5, 4, 5);
+            tbPassword.Name = "tbPassword";
+            tbPassword.Size = new Size(160, 27);
+            tbPassword.TabIndex = 2;
+            tbPassword.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(88, 60);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(111, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Enter Password:";
+            // 
+            // btnOk
+            // 
+            btnOk.DialogResult = DialogResult.OK;
+            btnOk.Location = new Point(398, 126);
+            btnOk.Margin = new Padding(4, 5, 4, 5);
+            btnOk.Name = "btnOk";
+            btnOk.Size = new Size(100, 35);
+            btnOk.TabIndex = 3;
+            btnOk.Text = "OK";
+            btnOk.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            btnCancel.DialogResult = DialogResult.Cancel;
+            btnCancel.Location = new Point(506, 126);
+            btnCancel.Margin = new Padding(4, 5, 4, 5);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(100, 35);
+            btnCancel.TabIndex = 4;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // PasswordForm
+            // 
+            AcceptButton = btnOk;
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = btnCancel;
+            ClientSize = new Size(617, 172);
+            Controls.Add(btnCancel);
+            Controls.Add(btnOk);
+            Controls.Add(label1);
+            Controls.Add(tbPassword);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Margin = new Padding(4, 5, 4, 5);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "PasswordForm";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "Password";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
