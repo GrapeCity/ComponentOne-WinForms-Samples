28d13e13bf5f70c90d6f0350ac57ce478de75b51
Sonam Dendup <sonam.dendup@developertools.com>
2024-09-06 05:32:21 +0000

    Merged in bugfix/C1-6866-fix-transpose-sample-bugs-new-UI (pull request #375)
    Bugfix/C1-6866 fix transpose sample bugs new UI
    
    * New TransposeSample V2 Overhaul
    
    * Learn more link added and package reference version with asterisk add
    
    * d file remove
    
    
    Approved-by: Denis Zmitrichenko

--- Core/FlexGrid/CS/TransposedGrid/Hotel.cs
+++ Core/FlexGrid/CS/TransposedGrid/Hotel.cs
@@ -3,44 +3,15 @@
     // Create a custom class Hotel
     internal class Hotel
     {
-        static Random _rnd = new Random();
-        static string[] _names = ["Dragon Hotel", "Tiger's Nest", "Wind Horse"];
-        static double[] _roomRevenue = [54000, 45000, 32000];
-        static double[] _restaurantRevenue = [50000, 46000, 49000];
-        static double[] _barRevenue = [23000, 43250, 10100];
-        static double[] _otherRevenue = [9800, 5500, 20200];
-        static double[] _grossRevenue = [136000, 139750, 111300];
-        static double[] _pat = [75600, 71388, 36900];
-        static double[] _expense = [52000, 60430, 70200];
-        static double[] _totalGuest = [821, 750, 668];
-        static double[] _totalStaff = [20, 23, 30];
-        static double[] _numberRooms = [21, 19, 15];
-        public Hotel(int i)
-        {
-            HotelName = _names[i];
-            RoomRevenue = _roomRevenue[i];
-            RestaurantRevenue = _restaurantRevenue[i];
-            BarRevenue = _barRevenue[i];
-            OtherRevenue = _otherRevenue[i];
-            GrossRevenue = _grossRevenue[i];
-            PAT = _pat[i];
-            Expense = _expense[i];
-            TotalGuests = _totalGuest[i];
-            TotalStaff = _totalStaff[i];
-            NumberRooms = _numberRooms[i];
-        }
         public string HotelName { get; set; }
         public double RoomRevenue { get; set; }
         public double RestaurantRevenue { get; set; }
-
         public double BarRevenue { get; set; }
-
         public double OtherRevenue { get; set; }
         public double GrossRevenue { get; set; }
         public double PAT { get; set; }
         public double Expense { get; set; }
         public double TotalGuests { get; set; }
-
         public double TotalStaff { get; set; }
         public double NumberRooms { get; set; }
     }
--- Core/FlexGrid/CS/TransposedGrid/Properties/Resources.Designer.cs
+++ Core/FlexGrid/CS/TransposedGrid/Properties/Resources.Designer.cs
@@ -59,5 +59,25 @@ namespace TransposedGrid.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap transpose_off {
+            get {
+                object obj = ResourceManager.GetObject("transpose-off", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap transpose_on {
+            get {
+                object obj = ResourceManager.GetObject("transpose-on", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
