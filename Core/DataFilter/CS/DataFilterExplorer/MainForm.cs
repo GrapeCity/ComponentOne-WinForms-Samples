1e960f52b714ed88142a48bc0d7f9d70e0dd6bcb
Tenzin Gyeltshen <tenzin.gyeltshen@developertools.com>
2024-11-13 09:51:01 +0000

    Merged in bugfix/C1WIN-32938-DataFilter (pull request #419)
    [Core] DataFilterExplorer [Theme Issue]
    
    * fixed
    
    
    Approved-by: Sonam Dendup

--- Core/DataFilter/CS/DataFilterExplorer/MainForm.cs
+++ Core/DataFilter/CS/DataFilterExplorer/MainForm.cs
@@ -1,6 +1,5 @@
 ﻿using C1.Win.Themes;
 using DataFilterExplorer.Data;
-using DataFilterExplorer.Samples;
 using System;
 using System.Linq;
 using System.Windows.Forms;
@@ -9,9 +8,6 @@ namespace DataFilterExplorer
 {
     public partial class MainForm : Form
     {
-        public string SelectedTheme { get; set; }
-
-
         public MainForm()
         {
             InitializeComponent();
@@ -23,11 +19,11 @@ namespace DataFilterExplorer
 
             var themes = C1ThemeController.GetThemes();
             cmbThemes.Items.Add("(none)");
-            foreach(var theme in themes)
+            foreach (var theme in themes)
             {
                 cmbThemes.Items.Add(theme);
             }
-            cmbThemes.SelectedIndex = cmbThemes.Items.Select(x => 
+            cmbThemes.SelectedIndex = cmbThemes.Items.Select(x =>
                 x.DisplayText).ToList().IndexOf("Office365White");
         }
 
@@ -38,32 +34,36 @@ namespace DataFilterExplorer
             var sample = lblSamples.SelectedItem as SampleItem;
             lblTitle.Text = sample.Title;
             lblDescription.Text = sample.Description;
-            // Pass MainForm reference when creating the sample control
-            Control control = sample.Name switch
-            {
-                "FilterEditorAndDataEngine" => new FilterEditorAndDataEngine(),
-                "DataFilterAndDataEngine" => new DataFilterAndDataEngine(),
-                _ => sample.Sample
-            };
-
-            if (control is FilterEditorAndDataEngine filterEditorControl)
-            {
-                filterEditorControl.SelectedTheme = this.SelectedTheme;  // Replace desiredTheme with the theme you want to set
-            }
-            else if (control is DataFilterAndDataEngine dataFilterControl)
-            {
-                dataFilterControl.SelectedTheme = this.SelectedTheme;
-            }
-
+            var control = sample.Sample;
             control.Dock = DockStyle.Fill;
             this.pnlSample.Controls.Add(control);
+
+            ApplyTheme();
+
             if (control is Form form)
             {
                 form.Show();
             }
+        }
 
-            ApplyThemeToCurrentSample(); // Apply theme when a new sample is selected
+        private void ApplyTheme()
+        {
+            if (cmbThemes.SelectedIndex == -1)
+                return;
 
+            var selectedTheme = C1ThemeController.GetThemeByName((string)cmbThemes.SelectedItem.Value, false);
+            C1ThemeController.ApplyThemeToControlTree(pnlSample, selectedTheme, null, true);
+
+            var sample = lblSamples.SelectedItem as SampleItem;
+            if (sample != null)
+            {
+                if (sample.Name == "FilterEditorAndDataEngine" && pnlSample.Controls[0] is Samples.FilterEditorAndDataEngine sampleFD)
+                    sampleFD.ApplyTheme(selectedTheme);
+                else if (sample.Name == "FilterEditorSummaryPanel" && pnlSample.Controls[0] is Samples.FilterEditorSummaryPanel sampleES)
+                    sampleES.ApplyTheme(selectedTheme);
+                else if (sample.Name == "UnboundMode" && pnlSample.Controls[0] is Samples.UnboundMode sampleUM)
+                    sampleUM.ApplyTheme(selectedTheme);
+            }
         }
 
         private void chkInfo_CheckedChanged(object sender, EventArgs e)
@@ -73,41 +73,7 @@ namespace DataFilterExplorer
 
         private void cmbThemes_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (cmbThemes.SelectedIndex == -1)
-                return;
-            SelectedTheme = cmbThemes.SelectedItem.DisplayText;
-            ApplyThemeToCurrentSample(); // Apply the selected theme to the current sample
-        }
-
-        private void ApplyThemeToCurrentSample()
-        {
-            if (string.IsNullOrEmpty(SelectedTheme) || SelectedTheme == "(none)")
-                return;
-
-            var theme = C1ThemeController.GetThemeByName(SelectedTheme, false);
-            C1ThemeController.ApplyThemeToControlTree(pnlSample, theme, null, true);
-
-            var sample = lblSamples.SelectedItem as SampleItem;
-            if (sample != null)
-            {
-                switch (sample.Name)
-                {
-                    case "FilterEditorAndDataEngine":
-                        if (pnlSample.Controls[0] is Samples.FilterEditorAndDataEngine sampleFD)
-                            sampleFD.ApplyTheme(theme);
-                        break;
-
-                    case "FilterEditorSummaryPanel":
-                        if (pnlSample.Controls[0] is Samples.FilterEditorSummaryPanel sampleES)
-                            sampleES.ApplyTheme(theme);
-                        break;
-
-                    case "UnboundMode":
-                        if (pnlSample.Controls[0] is Samples.UnboundMode sampleUM)
-                            sampleUM.ApplyTheme(theme);
-                        break;
-                }
-            }
+            ApplyTheme();
         }
     }
 }
