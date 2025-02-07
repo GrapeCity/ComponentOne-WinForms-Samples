dda27fac09727767532cbe0bca9310ba9f2a34c6
Karsel Dawa <karsel.dawa@developertools.com>
2025-02-07 03:40:36 +0000

    Merged in bugfix/C1-7571-sample4.8-observed-designer-erro (pull request #524)
    Fixed Designer issue
    
    * Fixed Designer issue
    
    
    Approved-by: Sonam Dendup
    Approved-by: Vyacheslav Batushin
    Approved-by: Oleg Golikov

--- NetFramework/FlexReport/CS/EmbeddedFonts/Form1.Designer.cs
+++ NetFramework/FlexReport/CS/EmbeddedFonts/Form1.Designer.cs
@@ -42,10 +42,10 @@ namespace EmbeddedFonts
             // 
             // btnLoad
             // 
-            this.btnLoad.Location = new System.Drawing.Point(20, 0);
-            this.btnLoad.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnLoad.Location = new System.Drawing.Point(18, 0);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLoad.Name = "btnLoad";
-            this.btnLoad.Size = new System.Drawing.Size(104, 35);
+            this.btnLoad.Size = new System.Drawing.Size(92, 28);
             this.btnLoad.TabIndex = 1;
             this.btnLoad.Text = "Load...";
             this.btnLoad.UseVisualStyleBackColor = true;
@@ -53,10 +53,10 @@ namespace EmbeddedFonts
             // 
             // btnSaveAs
             // 
-            this.btnSaveAs.Location = new System.Drawing.Point(132, 1);
-            this.btnSaveAs.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSaveAs.Location = new System.Drawing.Point(117, 1);
+            this.btnSaveAs.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSaveAs.Name = "btnSaveAs";
-            this.btnSaveAs.Size = new System.Drawing.Size(109, 35);
+            this.btnSaveAs.Size = new System.Drawing.Size(97, 28);
             this.btnSaveAs.TabIndex = 2;
             this.btnSaveAs.Text = "Save as...";
             this.btnSaveAs.UseVisualStyleBackColor = true;
@@ -68,9 +68,10 @@ namespace EmbeddedFonts
             this.c1ComboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.c1ComboBox1.GapHeight = 0;
             this.c1ComboBox1.ImagePadding = new System.Windows.Forms.Padding(0);
-            this.c1ComboBox1.Location = new System.Drawing.Point(248, 4);
+            this.c1ComboBox1.Location = new System.Drawing.Point(220, 3);
+            this.c1ComboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.c1ComboBox1.Name = "c1ComboBox1";
-            this.c1ComboBox1.Size = new System.Drawing.Size(280, 28);
+            this.c1ComboBox1.Size = new System.Drawing.Size(249, 26);
             this.c1ComboBox1.Style.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.c1ComboBox1.TabIndex = 4;
             this.c1ComboBox1.Tag = null;
@@ -85,9 +86,10 @@ namespace EmbeddedFonts
             // 
             this.c1MultiSelect1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.c1MultiSelect1.Location = new System.Drawing.Point(534, 4);
+            this.c1MultiSelect1.Location = new System.Drawing.Point(475, 3);
+            this.c1MultiSelect1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.c1MultiSelect1.Name = "c1MultiSelect1";
-            this.c1MultiSelect1.Size = new System.Drawing.Size(654, 32);
+            this.c1MultiSelect1.Size = new System.Drawing.Size(581, 26);
             this.c1MultiSelect1.TabIndex = 5;
             this.c1MultiSelect1.SelectionChanged += new System.EventHandler(this.c1MultiSelect1_SelectionChanged);
             // 
@@ -98,23 +100,24 @@ namespace EmbeddedFonts
             | System.Windows.Forms.AnchorStyles.Right)));
             this.c1FlexViewer1.AutoScrollMargin = new System.Drawing.Size(0, 0);
             this.c1FlexViewer1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
-            this.c1FlexViewer1.Location = new System.Drawing.Point(0, 45);
-            this.c1FlexViewer1.Margin = new System.Windows.Forms.Padding(6);
+            this.c1FlexViewer1.Location = new System.Drawing.Point(0, 36);
+            this.c1FlexViewer1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.c1FlexViewer1.Name = "c1FlexViewer1";
-            this.c1FlexViewer1.Size = new System.Drawing.Size(1202, 646);
+            this.c1FlexViewer1.Size = new System.Drawing.Size(1068, 517);
             this.c1FlexViewer1.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.c1MultiSelect1);
             this.Controls.Add(this.c1ComboBox1);
             this.Controls.Add(this.btnSaveAs);
             this.Controls.Add(this.btnLoad);
             this.Controls.Add(this.c1FlexViewer1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Embedded fonts (you can choose any font from the second list to embed)";
             this.Load += new System.EventHandler(this.Form1_Load);
