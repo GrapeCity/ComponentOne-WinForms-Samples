49030e764bd0ff0ed74f633b3f5e29d90402f4aa
Tenzin Gyeltshen <tenzin.gyeltshen@developertools.com>
2025-02-28 11:58:30 +0000

    Merged in bugfix/C1-7643-dropdown-bug (pull request #552)
    dropdown layout defect
    
    * fixed autosize
    
    
    Approved-by: Sonam Dendup

--- NetFramework/WinForms/CS/ControlExplorer/Input/CustomDropDowns.Designer.cs
+++ NetFramework/WinForms/CS/ControlExplorer/Input/CustomDropDowns.Designer.cs
@@ -190,6 +190,7 @@
             // 
             // CustomDropDowns
             // 
+            this.AutoSize = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
