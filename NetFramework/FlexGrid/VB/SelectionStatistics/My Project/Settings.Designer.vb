3ee6fe593e5e6d198c59b6d02f744593e966a6d3
Karsel Dawa <karsel.dawa@developertools.com>
2025-03-07 11:26:14 +0000

    Merged in bugfix/C1-7570-sample4.8-observed-designer-erro (pull request #553)
    Bugfix/C1-7570 sample4.8 observed designer erro
    
    * fixed Designer issue
    
    * changed Icon and platformtarget
    
    
    Approved-by: Maxim Loverov
    Approved-by: Sonam Dendup
    Approved-by: Irina Pykhova

--- NetFramework/FlexGrid/VB/SelectionStatistics/My Project/Settings.Designer.vb
+++ NetFramework/FlexGrid/VB/SelectionStatistics/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.13.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -57,7 +57,7 @@ Namespace My
         <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\C1NWind.mdb")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\C1NWind.mdb")>  _
         Public ReadOnly Property C1NWindConnectionString() As String
             Get
                 Return CType(Me("C1NWindConnectionString"),String)
