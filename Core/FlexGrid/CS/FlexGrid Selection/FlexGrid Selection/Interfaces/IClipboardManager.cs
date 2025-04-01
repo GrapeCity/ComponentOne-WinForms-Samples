ea9d54204137250fe8d70528d59a8f11103d1b3e
Karsel Dawa <karsel.dawa@developertools.com>
2025-04-01 11:24:38 +0000

    Merged in feature/C1-7619-demo-flexgrid-selection-final-de (pull request #585)
    Feature/C1-7619 demo flexgrid selection final de
    
    * Fixed ribbon controls gettings freezed
    
    * fixed aggregate panel and flexgrid
    
    * making about section
    
    * made changes on about section
    
    * connected about with form1
    
    * made spacing in ribbon, changed icon, disabling of selectALLBtn
    
    * modified aggregate group
    
    * Created a close button
    
    * refactored the code
    
    * added asynchronous programming
    
    * separated logic into different classes
    
    * made link between topic desription
    
    * refactored and updated all the code
    
    * created topicLinks class to handle hyperlinks
    removed unused resources
    
    * removed unused resources
    
    * removed about() from program.cs
    
    * changed event handling in About.cs, and how aggregate checkbox behaves
    
    * refactored the code to follow solid principle
    
    * added comment in form1
    
    * made an aggregatePanel
    
    * removed unnecessary comments
    
    * updated the comments
    
    * removed whitespaces
    
    * removed unnecessary namespaces
    
    * removed white spaces
    
    
    Approved-by: Sonam Dendup

---
+++ Core/FlexGrid/CS/FlexGrid Selection/FlexGrid Selection/Interfaces/IClipboardManager.cs
@@ -0,0 +1,12 @@
+﻿namespace FlexGrid_Selection.Interfaces
+{
+    public interface IClipboardManager
+    {
+        void Copy();
+        void Paste();
+        void AdjustGridForPaste();
+        void UpdateCopyMode();
+        void UpdateClipboardCopyModeText();
+        void ApplySearch();
+    }
+}
---
+++ Core/FlexGrid/CS/FlexGrid Selection/FlexGrid Selection/userControl/AggregatePanel.cs
@@ -0,0 +1,118 @@
+﻿using C1.Framework;
+using C1.Win.Input;
+
+namespace FlexGrid_Selection.userControl
+{
+    public partial class AggregatePanel : UserControl
+    {
+        public event EventHandler CloseButtonClicked;
+        public AggregatePanel()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+        }
+
+        /// <summary>
+        /// Updates the control with the given aggregate sections.
+        /// Each key is a sub-header (e.g., "Source FlexGrid Aggregates:"), and its value is a list of aggregate strings.
+        /// </summary>
+        public void UpdateAggregates(Dictionary<string, IEnumerable<string>> aggregateSections)
+        {
+            this.SuspendLayout();
+            this.Controls.Clear();
+
+            // Create and add the header label and close button.
+            var headerLabel = CreateHeaderLabel();
+            this.Controls.Add(headerLabel);
+            this.Controls.Add(CreateCloseButton());
+
+            int yPosition = 60; 
+
+            // For each section, create a sub-header and add aggregate result labels.
+            foreach (var section in aggregateSections)
+            {
+                var subHeader = CreateSubHeaderLabel(section.Key, new Point(10, yPosition));
+                this.Controls.Add(subHeader);
+                yPosition = subHeader.Bottom + 5;
+
+                foreach (var agg in section.Value)
+                {
+                    var resultLabel = new Label
+                    {
+                        Text = agg,
+                        AutoSize = true,
+                        Font = new Font("Segoe UI", 12F),
+                        Location = new Point(10, yPosition)
+                    };
+                    this.Controls.Add(resultLabel);
+                    yPosition += resultLabel.Height + 5;
+                }
+                yPosition += 10;
+            }
+            this.ResumeLayout(true);
+            this.Update();
+        }
+
+        /// <summary>
+        /// Clears all controls from the display.
+        /// </summary>
+        public void ClearDisplay()
+        {
+            this.SuspendLayout();
+            this.Controls.Clear();
+            this.ResumeLayout(true);
+        }
+
+        /// <summary>
+        /// Creates the header label.
+        /// </summary>
+        private C1Label CreateHeaderLabel()
+        {
+            return new C1Label
+            {
+                Text = "Aggregate Detail",
+                Font = new Font("Segoe UI", 14F, FontStyle.Bold),
+                Location = new Point(60, 8),
+                Size = new Size(230, 50)
+            };
+        }
+
+        /// <summary>
+        /// Creates a close button that hides the control when clicked.
+        /// </summary>
+        private C1Button CreateCloseButton()
+        {
+            var closeBtn = new C1Button
+            {
+                Icon = new C1BitmapIcon("CloseIcon", new Size(32, 32), Color.Transparent, Properties.Resources.ChromeClose),
+                Location = new Point(this.Width - 60, 8),
+                Size = new Size(42, 42),
+                Anchor = AnchorStyles.Top | AnchorStyles.Right,
+                ButtonType = C1.Win.Input.ButtonType.Default,
+            };
+
+            closeBtn.Styles.Border = new C1.Framework.Thickness(0);
+            closeBtn.Styles.Hot.BackColor = SystemColors.Control;
+            closeBtn.Click += (s, e) =>
+            {
+                this.Visible = false;
+                CloseButtonClicked?.Invoke(this, EventArgs.Empty);
+            };
+            return closeBtn;
+        }
+
+        /// <summary>
+        /// Creates a sub-header label for a section.
+        /// </summary>
+        private Label CreateSubHeaderLabel(string text, Point location)
+        {
+            return new Label
+            {
+                Text = text,
+                AutoSize = true,
+                Font = new Font("Segoe UI", 12F, FontStyle.Bold),
+                Location = location
+            };
+        }
+    }
+}
