13bb84991e590022c74db8f2f36daf38170f04ac
Karsel Dawa <karsel.dawa@developertools.com>
2025-04-23 03:29:12 +0000

    Merged in feature/C1-7765-flexgridexplorer-subtotals (pull request #598)
    added sample "Subtotals" in flexgridExplorer
    
    * added sample "Subtotals" in flexgridExplorer
    
    
    Approved-by: Maxim Loverov
    Approved-by: Sonam Dendup

---
+++ Core/FlexGrid/CS/FlexGridExplorer/Samples/Subtotals.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace FlexGridExplorer.Samples
+{
+    partial class Subtotals
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            c1FlexGrid1 = new C1.Win.FlexGrid.C1FlexGrid();
+            ((System.ComponentModel.ISupportInitialize)c1FlexGrid1).BeginInit();
+            SuspendLayout();
+            // 
+            // c1FlexGrid1
+            // 
+            c1FlexGrid1.ColumnInfo = "10,1,0,0,0,-1,Columns:";
+            c1FlexGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            c1FlexGrid1.Location = new System.Drawing.Point(0, 0);
+            c1FlexGrid1.Name = "c1FlexGrid1";
+            c1FlexGrid1.SelectionMode = C1.Win.FlexGrid.SelectionModeEnum.Row;
+            c1FlexGrid1.Size = new System.Drawing.Size(860, 733);
+            c1FlexGrid1.TabIndex = 0;
+            // 
+            // Subtotals
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Controls.Add(c1FlexGrid1);
+            Name = "Subtotals";
+            Size = new System.Drawing.Size(860, 733);
+            Load += NewSubtotals_Load;
+            ((System.ComponentModel.ISupportInitialize)c1FlexGrid1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private C1.Win.FlexGrid.C1FlexGrid c1FlexGrid1;
+    }
+}
---
+++ Core/FlexGrid/CS/FlexGridExplorer/Samples/Subtotals.cs
@@ -0,0 +1,109 @@
+﻿using C1.Win.FlexGrid;
+using FlexGridExplorer.Data;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FlexGridExplorer.Samples
+{
+    public partial class Subtotals : UserControl
+    {
+        private DataView _dataView;
+        public Subtotals()
+        {
+            InitializeComponent();
+        }
+
+        private void NewSubtotals_Load(object sender, EventArgs e)
+        {
+            c1FlexGrid1.BeginUpdate();
+            var sql = @"
+                        SELECT 
+                            Employees.FirstName || ' ' || Employees.LastName AS Salesperson,
+                            Customers.Country,
+                            Shippers.CompanyName,
+                            Products.ProductName,
+                            Orders.OrderDate,
+                            CAST([Order Details].UnitPrice * [Order Details].Quantity * (1 - [Order Details].Discount) AS NUMERIC) AS Amount
+                        FROM Orders
+                        INNER JOIN Shippers ON Shippers.ShipperID = Orders.ShipVia
+                        INNER JOIN Customers ON Customers.CustomerID = Orders.CustomerID
+                        INNER JOIN Employees ON Employees.EmployeeID = Orders.EmployeeID
+                        INNER JOIN [Order Details] ON Orders.OrderID = [Order Details].OrderID
+                        INNER JOIN Products ON Products.ProductID = [Order Details].ProductID;
+                        ";
+            DataTable originalData = DataSource.GetRows(sql);
+            _dataView = new DataView(originalData);
+
+            // Set the DataView as the data source
+            c1FlexGrid1.DataSource = _dataView;
+
+            // set up grid layout/behavior
+            c1FlexGrid1.AllowSorting = AllowSortingEnum.None;
+            c1FlexGrid1.AllowMerging = AllowMergingEnum.Nodes;
+            c1FlexGrid1.ExtendLastCol = true;
+            c1FlexGrid1.Cols[0].Width = 25;
+            c1FlexGrid1.Tree.Style = TreeStyleFlags.Simple;
+            c1FlexGrid1.Tree.Column = 1;
+
+            // set up grid styles
+            c1FlexGrid1.Styles.Normal.Border.Style = BorderStyleEnum.None;
+            c1FlexGrid1.Styles.Normal.Trimming = StringTrimming.EllipsisCharacter;
+
+            // right align Amount column
+            c1FlexGrid1.Cols["Amount"].TextAlign = TextAlignEnum.RightCenter;
+            c1FlexGrid1.Cols["Amount"].TextAlignFixed = TextAlignEnum.RightCenter;
+            c1FlexGrid1.Cols["Amount"].Format = "C";
+
+            // prevent user from dragging last three columns
+            c1FlexGrid1.Cols[4].AllowDragging = false;
+            c1FlexGrid1.Cols[5].AllowDragging = false;
+            c1FlexGrid1.Cols[6].AllowDragging = false;
+
+            // apply default sort order
+            Sort();
+            c1FlexGrid1_AfterDataRefresh(null, null);
+            c1FlexGrid1.EndUpdate();
+        }
+
+        private void c1FlexGrid1_AfterDataRefresh(object sender, ListChangedEventArgs e)
+        {
+            // total on Amount
+            int totalOn = c1FlexGrid1.Cols.Count - 1;
+            string caption = "Total for {0}";
+
+            // calculate three levels of totals
+            c1FlexGrid1.Subtotal(AggregateEnum.Sum, 0, 1, totalOn, caption);
+            c1FlexGrid1.Subtotal(AggregateEnum.Sum, 1, 2, totalOn, caption);
+            c1FlexGrid1.Subtotal(AggregateEnum.Sum, 2, 3, totalOn, caption);
+
+            //int totalOn = fg.Cols.Count - 1;
+            c1FlexGrid1.Subtotal(AggregateEnum.Sum, -1, -1, totalOn, "Grand Total");
+            c1FlexGrid1.Tree.Show(1);
+
+            // auto size the grid to accommodate tree
+            c1FlexGrid1.AutoSizeCols();
+            c1FlexGrid1.AutoSizeCols(1, 1, 1000, 3, 30, AutoSizeFlags.IgnoreHidden);
+        }
+
+        private void c1FlexGrid1_AfterDragColumn(object sender, DragRowColEventArgs e)
+        {
+            // Sort the recordset when the user drags columns.
+            // This will cause a data refresh, removing all subtotals and
+            // firing the AfterDataRefresh event, which rebuilds the subtotals.
+            Sort();
+
+            // Rebuild the subtotals and tree
+            c1FlexGrid1_AfterDataRefresh(null, null);
+        }
+
+        private void Sort()
+        {
+            // Apply sort to the DataView
+            string sortExpression = c1FlexGrid1.Cols[1].Name + ", " + c1FlexGrid1.Cols[2].Name + ", " + c1FlexGrid1.Cols[3].Name;
+            _dataView.Sort = sortExpression;
+        }
+    }
+}
