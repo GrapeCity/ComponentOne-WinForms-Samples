86f2b937725692f75f31b2210622eed190d5f138
Sonam Dendup <sonam.dendup@developertools.com>
2025-06-23 03:09:11 +0000

    Merged in feature/C1-7893-winformsdemocreate-new-flexgrid- (pull request #637)
    Feature/C1-7893 winformsdemocreate new flexgrid
    
    * Changed project structure for TransposeGrid
    
    * Broken Sample Description into logical porition for readibility & renamed TransposeGridForm to Transpose class
    
    * Changed property to Method for creating new instance of control

---
+++ Core/FlexGrid/CS/FlexGridExplorer/Samples/TransposedGrid/AboutFlexGridModalForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace FlexGridExplorer.Samples
+{
+    partial class AboutFlexGridModalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutFlexGridModalForm));
+            label5 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            linkLabel1 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            SuspendLayout();
+            // 
+            // label5
+            // 
+            label5.Location = new Point(28, 102);
+            label5.Margin = new Padding(5, 0, 5, 0);
+            label5.MaximumSize = new Size(1056, 320);
+            label5.Name = "label5";
+            label5.Size = new Size(1056, 253);
+            label5.TabIndex = 7;
+            label5.Text = resources.GetString("label5.Text");
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ControlDarkDark;
+            label2.Location = new Point(28, 30);
+            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(246, 40);
+            label2.TabIndex = 8;
+            label2.Text = "What is FlexGrid?";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ControlDarkDark;
+            label1.Location = new Point(28, 430);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(174, 40);
+            label1.TabIndex = 9;
+            label1.Text = "Learn More:";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.LinkColor = Color.DodgerBlue;
+            linkLabel1.Location = new Point(43, 503);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(138, 32);
+            linkLabel1.TabIndex = 10;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "1. Overview";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.LinkColor = Color.DodgerBlue;
+            linkLabel2.Location = new Point(43, 564);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(165, 32);
+            linkLabel2.TabIndex = 11;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "2. Key Feature";
+            linkLabel2.LinkClicked += linkLabel2_LinkClicked;
+            // 
+            // AboutFlexGridModalForm
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1100, 816);
+            Controls.Add(linkLabel2);
+            Controls.Add(linkLabel1);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(label5);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Margin = new Padding(5);
+            Name = "AboutFlexGridModalForm";
+            Opacity = 0.95D;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "About ComponentOne FlexGrid";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label5;
+        private Label label2;
+        private Label label1;
+        private LinkLabel linkLabel1;
+        private LinkLabel linkLabel2;
+    }
+}
