3ed972873afb6acd9554908d993375e8b9ea429b
alex.trefilov <alex.trefilov@developertools.com>
2025-09-19 10:12:06 +0200

    added CreatingDiagram sample

---
+++ Core/FlexDiagram/CS/CreatingDiagram/DiagramFactory.cs
@@ -0,0 +1,114 @@
+﻿using C1.Chart;
+using C1.Diagram;
+using C1.Diagram.Parser;
+using C1.Win.Diagram;
+using CreatingDiagram.Data;
+
+namespace CreatingDiagram
+{
+    public static class DiagramFactory
+    {
+        public static void Clear(FlexDiagram diagram)
+        {
+            ((IDiagram)diagram).Clear();
+            diagram.Direction = DiagramDirection.TopBottom;
+        }
+
+        public static Control? CreateUnboundDiagram(FlexDiagram diagram)
+        {
+            Clear(diagram);
+
+            var startNode = new Node() { Text = "Start Process" };
+            var processNode = new Node() { Text = "Execute", Shape = Shape.RoundedRectangle };
+            var endNode = new Node() { Text = "Complete" };
+
+            diagram.Nodes.Add(startNode);
+            diagram.Nodes.Add(processNode);
+            diagram.Nodes.Add(endNode);
+
+            diagram.Edges.Add(new Edge()
+            {
+                Source = startNode,
+                Target = processNode,
+                TargetArrow = ArrowStyle.Normal
+            });
+
+            diagram.Edges.Add(new Edge()
+            {
+                Source = processNode,
+                Target = endNode,
+                TargetArrow = ArrowStyle.Normal
+            });
+
+            return null;
+        }
+
+        public static Control? CreateBoundDiagram(FlexDiagram diagram)
+        {
+            Clear(diagram);
+            diagram.Direction = DiagramDirection.LeftRight;
+
+            var dataSource = DataService.CreateHierarchicalData();
+
+            diagram.DataSource = dataSource;
+            diagram.Binding = "Type";
+            diagram.ChildItemsPath = "Items";
+
+
+            var treeView = new TreeView() { Dock = DockStyle.Fill, BorderStyle = BorderStyle.None };
+            treeView.Nodes.Clear();
+
+            foreach (var item in dataSource)
+            {
+                var node = treeView.Nodes.Add(item.Type);
+                AddItems(node, item);
+            }
+
+            treeView.ExpandAll();
+
+            return treeView;
+        }
+
+        static void AddItems(TreeNode node, SalesDataItem item)
+        {
+            if (item.Items == null)
+                return;
+
+            foreach (var n in item.Items)
+            {
+                var child = node.Nodes.Add(n.Type);
+                AddItems(child, n);
+            }
+        }
+
+        public static Control? CreateTableBoundDiagram(FlexDiagram diagram)
+        {
+            Clear(diagram);
+
+            var table = DataService.CreateSkillsTable();
+
+            diagram.Direction = DiagramDirection.LeftRight;
+            diagram.DataSource = table;
+            diagram.Binding = "Field,Domain,Specialty,Skill";
+
+            var grid = new DataGridView() { Dock = DockStyle.Fill, ReadOnly = true, AllowUserToAddRows = false, ColumnHeadersVisible = false };
+            grid.DataSource = table;
+
+            return grid;
+        }
+
+        public static Control? CreateMermaidDiagram(FlexDiagram diagram)
+        {
+            Clear(diagram);
+
+            var text = File.ReadAllText("Resources\\pizza.txt");
+            diagram.LoadMermaidGraph(text);
+
+            var textBox = new TextBox() { Multiline = true, Dock = DockStyle.Fill };
+            textBox.Font = new Font("Consolas", diagram.Font.Size);
+            textBox.Text = text;
+
+            return textBox;
+        }
+    }
+}
---
+++ Core/FlexDiagram/CS/CreatingDiagram/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace CreatingDiagram
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            flexDiagram1 = new C1.Win.Diagram.FlexDiagram();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            codeSnippet1 = new CreatingDiagram.Controls.CodeSnippet();
+            tabPage2 = new TabPage();
+            toolStrip1 = new ToolStrip();
+            buttonBack = new ToolStripButton();
+            toolStripLabel1 = new ToolStripLabel();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            tableLayoutPanel1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            toolStrip1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(flexDiagram1, 0, 1);
+            tableLayoutPanel1.Controls.Add(tabControl1, 1, 1);
+            tableLayoutPanel1.Controls.Add(toolStrip1, 0, 0);
+            tableLayoutPanel1.Controls.Add(statusStrip1, 0, 2);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Margin = new Padding(4, 6, 4, 6);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 38F));
+            tableLayoutPanel1.Size = new Size(1257, 840);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flexDiagram1
+            // 
+            flexDiagram1.Binding = "";
+            flexDiagram1.ChildItemsPath = "";
+            flexDiagram1.Direction = C1.Diagram.DiagramDirection.TopBottom;
+            flexDiagram1.Dock = DockStyle.Fill;
+            flexDiagram1.EdgeRouting = C1.Diagram.EdgeRouting.Rectilinear;
+            flexDiagram1.Font = new Font("Segoe UI Emoji", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            flexDiagram1.InteractionMode = C1.Diagram.InteractionMode.None;
+            flexDiagram1.Legend.ItemMaxWidth = 0;
+            flexDiagram1.Legend.Orientation = C1.Chart.Orientation.Auto;
+            flexDiagram1.Legend.Position = C1.Chart.Position.Bottom;
+            flexDiagram1.Legend.Reversed = false;
+            flexDiagram1.Legend.ScrollBars = C1.Chart.LegendScrollBars.None;
+            flexDiagram1.Legend.TextWrapping = C1.Chart.TextWrapping.None;
+            flexDiagram1.Legend.Title = null;
+            flexDiagram1.Location = new Point(4, 55);
+            flexDiagram1.Margin = new Padding(4, 6, 4, 6);
+            flexDiagram1.Name = "flexDiagram1";
+            flexDiagram1.ScaleFactor = 6175F;
+            flexDiagram1.ScaleMode = C1.Diagram.ScaleMode.ScaleToFit;
+            flexDiagram1.SelectionStyle.StrokeColor = Color.Red;
+            flexDiagram1.Size = new Size(620, 741);
+            flexDiagram1.TabIndex = 0;
+            flexDiagram1.Text = "flexDiagram1";
+            // 
+            // 
+            // 
+            flexDiagram1.ToolTip.Content = "";
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(632, 55);
+            tabControl1.Margin = new Padding(4, 6, 4, 6);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(621, 741);
+            tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(codeSnippet1);
+            tabPage1.Location = new Point(4, 37);
+            tabPage1.Margin = new Padding(4, 6, 4, 6);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(4, 6, 4, 6);
+            tabPage1.Size = new Size(613, 700);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Code";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // codeSnippet1
+            // 
+            codeSnippet1.Code = null;
+            codeSnippet1.Dock = DockStyle.Fill;
+            codeSnippet1.Location = new Point(4, 6);
+            codeSnippet1.Margin = new Padding(4, 7, 4, 7);
+            codeSnippet1.Name = "codeSnippet1";
+            codeSnippet1.Size = new Size(605, 688);
+            codeSnippet1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Margin = new Padding(4, 6, 4, 6);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(4, 6, 4, 6);
+            tabPage2.Size = new Size(613, 704);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Data";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip1
+            // 
+            tableLayoutPanel1.SetColumnSpan(toolStrip1, 2);
+            toolStrip1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            toolStrip1.ImageScalingSize = new Size(20, 20);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { buttonBack, toolStripLabel1 });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(1257, 49);
+            toolStrip1.TabIndex = 2;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // buttonBack
+            // 
+            buttonBack.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            buttonBack.ImageTransparentColor = Color.Magenta;
+            buttonBack.Name = "buttonBack";
+            buttonBack.Size = new Size(53, 50);
+            buttonBack.Text = "←";
+            buttonBack.ToolTipText = "Back to Overview";
+            buttonBack.Visible = false;
+            buttonBack.Click += buttonBack_Click;
+            // 
+            // toolStripLabel1
+            // 
+            toolStripLabel1.Name = "toolStripLabel1";
+            toolStripLabel1.Size = new Size(159, 46);
+            toolStripLabel1.Text = "Overview";
+            // 
+            // statusStrip1
+            // 
+            tableLayoutPanel1.SetColumnSpan(statusStrip1, 2);
+            statusStrip1.ImageScalingSize = new Size(20, 20);
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 818);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Padding = new Padding(1, 0, 22, 0);
+            statusStrip1.Size = new Size(1257, 22);
+            statusStrip1.TabIndex = 3;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(0, 16);
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 28F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1257, 840);
+            Controls.Add(tableLayoutPanel1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(4, 6, 4, 6);
+            Name = "Form1";
+            Text = "Creating FlexDiagram";
+            Load += Form1_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private C1.Win.Diagram.FlexDiagram flexDiagram1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private ToolStrip toolStrip1;
+        private ToolStripButton buttonBack;
+        private ToolStripLabel toolStripLabel1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private TabPage tabPage2;
+        private Controls.CodeSnippet codeSnippet1;
+    }
+}
