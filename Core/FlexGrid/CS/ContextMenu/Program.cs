1532dcc9b23e26f9d51016e333771480798ab46a
Karsel Dawa <karsel.dawa@developertools.com>
2025-09-19 03:31:35 +0000

    Merged in bugfix/C1-8444-sample-text-in-rename-dialog-box (pull request #679)
    Text displayed under all DPI setting's
    
    * Made sure that all the text's are displayed under all DPI setting's
    
    
    Approved-by: Sonam Choki
    Approved-by: Tenzin Gyeltshen

--- Core/FlexGrid/CS/ContextMenu/Program.cs
+++ Core/FlexGrid/CS/ContextMenu/Program.cs
@@ -10,6 +10,7 @@ namespace ContextMenu
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
