a580b9df0c94a6c2baa3e9a0d07d76764d271380
Kinely Penjor <kinley.penjor@developertools.com>
2025-09-24 12:10:46 +0000

    Merged in feature/C1-8478-new-sample-winforms-flexgrid-filterdefination-ai-implementation (pull request #681)
    feat: New Sample Addition to the Flexgrid FilterDefination Property. add: Gemini and Groq ai to response the natural language propmt to a filter defination propety which is applied to the flexgrid filter defination property
    
    * feat: New Sample Addition to the Flexgrid FilterDefination Property. add: Gemini and Groq ai to response the natural language propmt to a filter defination propety which is applied to the flexgrid filter defination property
    
    * remove: removed gitignore and git attributes
    
    * remove: Unnecessary comments in the code
    
    * replace: List<KeyValuePair> with Dictionary
    
    * refact: Service code refact, removed unncessary comments.
    
    * refact: Moved to field region from the outer field
    
    * add: Added specific exception's
    
    * add: Exception Handling & refactor: refactor the Method

---
+++ Core/FlexGrid/CS/FilterDefinationAi/ErrorHandling/ErrorHandler.cs
@@ -0,0 +1,31 @@
+﻿using FlexGridGeminiAI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexGridGeminiAI.ErrorHandling
+{
+    public static class ErrorHandler
+    {
+        public static AppError FromHttpStatus(int code, string? details = null)
+        {
+            return code switch
+            {
+                401 => new AppError(ErrorCode.ApiUnauthorized, "Unauthorized access.", details),
+                402 => new AppError(ErrorCode.ApiPaymentRequired, "Payment required.", details),
+                403 => new AppError(ErrorCode.ApiForbidden, "Access is forbidden.", details),
+                404 => new AppError(ErrorCode.ApiNotFound, "Resource not found.", details),
+                500 => new AppError(ErrorCode.ApiInternalError, "Internal server error.", details),
+                503 => new AppError(ErrorCode.ApiServiceUnavailable, "Service is unavailable.", details),
+                _ => new AppError(ErrorCode.ApiInternalError, $"Unhandled status code: {code}", details)
+
+            };
+        }
+        public static AppError FromModelError(ErrorCode code, string message, string? details = null, Exception? ex = null)
+        {
+            return new AppError(code, message, details, ex);
+        }
+    }
+}
---
+++ Core/FlexGrid/CS/FilterDefinationAi/Interface/IPromptGenerator.cs
@@ -0,0 +1,14 @@
+﻿using FlexGridGeminiAI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexGridGeminiAI.Interface
+{
+    public interface IPromptGenerator
+    {
+        public string GenerateGeminiPrompt(string userPrompt, List<DataColumnSchema> schema);
+    }
+}
