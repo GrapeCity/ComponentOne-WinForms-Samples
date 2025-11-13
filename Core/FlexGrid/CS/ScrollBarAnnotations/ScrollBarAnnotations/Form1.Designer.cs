d0d981a127275f857008fa4a5a66f6fca8c895f2
Kinely Penjor <kinley.penjor@developertools.com>
2025-11-13 06:54:48 +0000

    Merged in bugfix/flexgrid-cs-scrollbarannotation-sample-border-of-combobox-is-cliped (pull request #697)
    Fixed the sample border of combo box is clipped
    
    * resolved: wrong commit on other sample
    
    * img: updaed the screenshot
    
    * stle: referenced to the resource file
    
    
    Approved-by: Tenzin Gyeltshen
    Approved-by: Sonam Choeda
    Approved-by: Ngawang Zepa

--- Core/FlexGrid/CS/ScrollBarAnnotations/ScrollBarAnnotations/Form1.Designer.cs
+++ Core/FlexGrid/CS/ScrollBarAnnotations/ScrollBarAnnotations/Form1.Designer.cs
@@ -35,8 +35,6 @@ namespace ScrollBarAnnotations
             _flexGrid = new C1.Win.FlexGrid.C1FlexGrid();
             c1ThemeController = new C1.Win.Themes.C1ThemeController();
             topPanel = new Panel();
-            sampleDescription = new C1.Win.Input.C1Label();
-            sampleTitle = new C1.Win.Input.C1Label();
             themeComboBox = new C1.Win.Input.C1ComboBox();
             hotKeysCheckbox = new C1.Win.Input.C1CheckBox();
             showScrollTipCheckbox = new C1.Win.Input.C1CheckBox();
@@ -54,8 +52,6 @@ namespace ScrollBarAnnotations
             ((System.ComponentModel.ISupportInitialize)_flexGrid).BeginInit();
             ((System.ComponentModel.ISupportInitialize)c1ThemeController).BeginInit();
             topPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)sampleDescription).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)sampleTitle).BeginInit();
             ((System.ComponentModel.ISupportInitialize)themeComboBox).BeginInit();
             ((System.ComponentModel.ISupportInitialize)hotKeysCheckbox).BeginInit();
             ((System.ComponentModel.ISupportInitialize)showScrollTipCheckbox).BeginInit();
@@ -78,8 +74,9 @@ namespace ScrollBarAnnotations
             _flexGrid.Dock = DockStyle.Fill;
             _flexGrid.ExtendLastCol = true;
             _flexGrid.Location = new Point(0, 0);
+            _flexGrid.Margin = new Padding(2);
             _flexGrid.Name = "_flexGrid";
-            _flexGrid.Size = new Size(1396, 844);
+            _flexGrid.Size = new Size(1073, 754);
             _flexGrid.TabIndex = 0;
             _flexGrid.Tree.Indent = 18;
             // 
@@ -89,49 +86,24 @@ namespace ScrollBarAnnotations
             // 
             // topPanel
             // 
-            topPanel.Controls.Add(sampleDescription);
-            topPanel.Controls.Add(sampleTitle);
             topPanel.Controls.Add(themeComboBox);
             topPanel.Dock = DockStyle.Top;
             topPanel.Location = new Point(0, 0);
-            topPanel.Margin = new Padding(5);
+            topPanel.Margin = new Padding(4);
             topPanel.Name = "topPanel";
-            topPanel.Size = new Size(2000, 210);
+            topPanel.Size = new Size(1538, 69);
             topPanel.TabIndex = 3;
             c1ThemeController.SetTheme(topPanel, "(default)");
-            // 
-            // sampleDescription
-            // 
-            sampleDescription.Font = new Font("Segoe UI", 12F);
-            sampleDescription.Location = new Point(29, 122);
-            sampleDescription.MaximumSize = new Size(4001, 99);
-            sampleDescription.Name = "sampleDescription";
-            sampleDescription.Size = new Size(4001, 67);
-            sampleDescription.TabIndex = 13;
-            sampleDescription.Text = Properties.Resource.ScrollbarAnnotationsDescription;
-            c1ThemeController.SetTheme(sampleDescription, "(default)");
-            // 
-            // sampleTitle
-            // 
-            sampleTitle.AutoSize = true;
-            sampleTitle.DefaultIconSize = new Size(20, 16);
-            sampleTitle.Font = new Font("Segoe UI", 17F, FontStyle.Bold);
-            sampleTitle.Location = new Point(29, 51);
-            sampleTitle.Name = "sampleTitle";
-            sampleTitle.Size = new Size(696, 74);
-            sampleTitle.TabIndex = 12;
-            sampleTitle.Text = Properties.Resource.ScrollbarAnnotationsTitle;
-            c1ThemeController.SetTheme(sampleTitle, "(default)");
             // 
             // themeComboBox
             // 
             themeComboBox.Anchor = AnchorStyles.Right;
             themeComboBox.DropDownStyle = C1.Win.Input.DropDownStyle.DropDownList;
             themeComboBox.InitialSelection = C1.Win.Input.InitialSelection.CaretAtStart;
-            themeComboBox.Location = new Point(1581, 74);
+            themeComboBox.Location = new Point(1220, 19);
             themeComboBox.Name = "themeComboBox";
             themeComboBox.ReadOnly = true;
-            themeComboBox.Size = new Size(396, 40);
+            themeComboBox.Size = new Size(305, 33);
             themeComboBox.TabIndex = 14;
             c1ThemeController.SetTheme(themeComboBox, "(default)");
             themeComboBox.SelectedIndexChanged += CmbThemes_SelectedIndexChanged;
@@ -139,9 +111,9 @@ namespace ScrollBarAnnotations
             // hotKeysCheckbox
             // 
             hotKeysCheckbox.AutoSize = true;
-            hotKeysCheckbox.Location = new Point(13, 357);
+            hotKeysCheckbox.Location = new Point(11, 269);
             hotKeysCheckbox.Name = "hotKeysCheckbox";
-            hotKeysCheckbox.Size = new Size(298, 38);
+            hotKeysCheckbox.Size = new Size(223, 27);
             hotKeysCheckbox.TabIndex = 10;
             hotKeysCheckbox.Text = Properties.Resource.Hotkeys;
             c1ThemeController.SetTheme(hotKeysCheckbox, "(default)");
@@ -150,9 +122,9 @@ namespace ScrollBarAnnotations
             // showScrollTipCheckbox
             // 
             showScrollTipCheckbox.AutoSize = true;
-            showScrollTipCheckbox.Location = new Point(13, 401);
+            showScrollTipCheckbox.Location = new Point(11, 302);
             showScrollTipCheckbox.Name = "showScrollTipCheckbox";
-            showScrollTipCheckbox.Size = new Size(264, 38);
+            showScrollTipCheckbox.Size = new Size(196, 27);
             showScrollTipCheckbox.TabIndex = 9;
             showScrollTipCheckbox.Text = "Show Scroll ToolTip";
             c1ThemeController.SetTheme(showScrollTipCheckbox, "(default)");
@@ -161,9 +133,9 @@ namespace ScrollBarAnnotations
             // customAnnotation1Checkbox
             // 
             customAnnotation1Checkbox.AutoSize = true;
-            customAnnotation1Checkbox.Location = new Point(13, 313);
+            customAnnotation1Checkbox.Location = new Point(11, 236);
             customAnnotation1Checkbox.Name = "customAnnotation1Checkbox";
-            customAnnotation1Checkbox.Size = new Size(546, 38);
+            customAnnotation1Checkbox.Size = new Size(409, 27);
             customAnnotation1Checkbox.TabIndex = 8;
             customAnnotation1Checkbox.Text = Properties.Resource.CustomAnnotations;
             c1ThemeController.SetTheme(customAnnotation1Checkbox, "(default)");
@@ -174,9 +146,9 @@ namespace ScrollBarAnnotations
             showErrorsCheckbox.AutoSize = true;
             showErrorsCheckbox.Checked = true;
             showErrorsCheckbox.CheckState = CheckState.Checked;
-            showErrorsCheckbox.Location = new Point(13, 137);
+            showErrorsCheckbox.Location = new Point(11, 104);
             showErrorsCheckbox.Name = "showErrorsCheckbox";
-            showErrorsCheckbox.Size = new Size(181, 38);
+            showErrorsCheckbox.Size = new Size(137, 27);
             showErrorsCheckbox.TabIndex = 7;
             showErrorsCheckbox.Text = "Show Errors";
             c1ThemeController.SetTheme(showErrorsCheckbox, "(default)");
@@ -188,9 +160,9 @@ namespace ScrollBarAnnotations
             showAnnotationsCheckbox.AutoSize = true;
             showAnnotationsCheckbox.Checked = true;
             showAnnotationsCheckbox.CheckState = CheckState.Checked;
-            showAnnotationsCheckbox.Location = new Point(13, 93);
+            showAnnotationsCheckbox.Location = new Point(11, 71);
             showAnnotationsCheckbox.Name = "showAnnotationsCheckbox";
-            showAnnotationsCheckbox.Size = new Size(250, 38);
+            showAnnotationsCheckbox.Size = new Size(188, 27);
             showAnnotationsCheckbox.TabIndex = 3;
             showAnnotationsCheckbox.Text = Properties.Resource.ShowAnnotations;
             c1ThemeController.SetTheme(showAnnotationsCheckbox, "(default)");
@@ -202,10 +174,10 @@ namespace ScrollBarAnnotations
             bottomPanel.Controls.Add(_flexGrid);
             bottomPanel.Controls.Add(displayOptionPanel);
             bottomPanel.Dock = DockStyle.Fill;
-            bottomPanel.Location = new Point(0, 210);
-            bottomPanel.Margin = new Padding(5);
+            bottomPanel.Location = new Point(0, 69);
+            bottomPanel.Margin = new Padding(4);
             bottomPanel.Name = "bottomPanel";
-            bottomPanel.Size = new Size(2000, 844);
+            bottomPanel.Size = new Size(1538, 754);
             bottomPanel.TabIndex = 4;
             c1ThemeController.SetTheme(bottomPanel, "(default)");
             // 
@@ -213,9 +185,10 @@ namespace ScrollBarAnnotations
             // 
             displayOptionPanel.Controls.Add(rightPanel);
             displayOptionPanel.Dock = DockStyle.Right;
-            displayOptionPanel.Location = new Point(1396, 0);
+            displayOptionPanel.Location = new Point(1073, 0);
+            displayOptionPanel.Margin = new Padding(2);
             displayOptionPanel.Name = "displayOptionPanel";
-            displayOptionPanel.Size = new Size(604, 844);
+            displayOptionPanel.Size = new Size(465, 754);
             displayOptionPanel.TabIndex = 15;
             c1ThemeController.SetTheme(displayOptionPanel, "(default)");
             // 
@@ -234,9 +207,10 @@ namespace ScrollBarAnnotations
             rightPanel.Dock = DockStyle.Fill;
             rightPanel.FlowDirection = FlowDirection.TopDown;
             rightPanel.Location = new Point(0, 0);
+            rightPanel.Margin = new Padding(2);
             rightPanel.Name = "rightPanel";
-            rightPanel.Padding = new Padding(10);
-            rightPanel.Size = new Size(604, 844);
+            rightPanel.Padding = new Padding(8);
+            rightPanel.Size = new Size(465, 754);
             rightPanel.TabIndex = 1;
             c1ThemeController.SetTheme(rightPanel, "(default)");
             rightPanel.WrapContents = false;
@@ -246,9 +220,9 @@ namespace ScrollBarAnnotations
             displayOptionTitle.AutoSize = true;
             displayOptionTitle.DefaultIconSize = new Size(20, 16);
             displayOptionTitle.Font = new Font("Segoe UI", 17F, FontStyle.Bold);
-            displayOptionTitle.Location = new Point(13, 13);
+            displayOptionTitle.Location = new Point(11, 11);
             displayOptionTitle.Name = "displayOptionTitle";
-            displayOptionTitle.Size = new Size(382, 74);
+            displayOptionTitle.Size = new Size(281, 54);
             displayOptionTitle.TabIndex = 11;
             displayOptionTitle.Text = Properties.Resource.DisplayOptions;
             c1ThemeController.SetTheme(displayOptionTitle, "(default)");
@@ -258,9 +232,9 @@ namespace ScrollBarAnnotations
             showSelectedRowCheckbox.AutoSize = true;
             showSelectedRowCheckbox.Checked = true;
             showSelectedRowCheckbox.CheckState = CheckState.Checked;
-            showSelectedRowCheckbox.Location = new Point(13, 181);
+            showSelectedRowCheckbox.Location = new Point(11, 137);
             showSelectedRowCheckbox.Name = "showSelectedRowCheckbox";
-            showSelectedRowCheckbox.Size = new Size(273, 38);
+            showSelectedRowCheckbox.Size = new Size(204, 27);
             showSelectedRowCheckbox.TabIndex = 13;
             showSelectedRowCheckbox.Text = Properties.Resource.ShowSelectedRows;
             c1ThemeController.SetTheme(showSelectedRowCheckbox, "(default)");
@@ -272,9 +246,9 @@ namespace ScrollBarAnnotations
             showFocusedRowCheckbox.AutoSize = true;
             showFocusedRowCheckbox.Checked = true;
             showFocusedRowCheckbox.CheckState = CheckState.Checked;
-            showFocusedRowCheckbox.Location = new Point(13, 225);
+            showFocusedRowCheckbox.Location = new Point(11, 170);
             showFocusedRowCheckbox.Name = "showFocusedRowCheckbox";
-            showFocusedRowCheckbox.Size = new Size(260, 38);
+            showFocusedRowCheckbox.Size = new Size(196, 27);
             showFocusedRowCheckbox.TabIndex = 12;
             showFocusedRowCheckbox.Text = Properties.Resource.ShowFocusedRow;
             c1ThemeController.SetTheme(showFocusedRowCheckbox, "(default)");
@@ -284,9 +258,9 @@ namespace ScrollBarAnnotations
             // showHighlightedRowCheckbox
             // 
             showHighlightedRowCheckbox.AutoSize = true;
-            showHighlightedRowCheckbox.Location = new Point(13, 269);
+            showHighlightedRowCheckbox.Location = new Point(11, 203);
             showHighlightedRowCheckbox.Name = "showHighlightedRowCheckbox";
-            showHighlightedRowCheckbox.Size = new Size(520, 38);
+            showHighlightedRowCheckbox.Size = new Size(384, 27);
             showHighlightedRowCheckbox.TabIndex = 14;
             showHighlightedRowCheckbox.Text = Properties.Resource.ShowHighlightedRows;
             c1ThemeController.SetTheme(showHighlightedRowCheckbox, "(default)");
@@ -299,21 +273,20 @@ namespace ScrollBarAnnotations
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(2000, 1054);
+            ClientSize = new Size(1538, 823);
             Controls.Add(bottomPanel);
             Controls.Add(topPanel);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(2);
             Name = "Form1";
-            Text = Properties.Resource.ScrollbarAnnotations;
+            Text = Properties.Resource.ScrollbarAnnotationsTitle;
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)_flexGrid).EndInit();
             ((System.ComponentModel.ISupportInitialize)c1ThemeController).EndInit();
             topPanel.ResumeLayout(false);
             topPanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)sampleDescription).EndInit();
-            ((System.ComponentModel.ISupportInitialize)sampleTitle).EndInit();
             ((System.ComponentModel.ISupportInitialize)themeComboBox).EndInit();
             ((System.ComponentModel.ISupportInitialize)hotKeysCheckbox).EndInit();
             ((System.ComponentModel.ISupportInitialize)showScrollTipCheckbox).EndInit();
@@ -344,8 +317,6 @@ namespace ScrollBarAnnotations
         private C1.Win.Input.C1CheckBox hotKeysCheckbox;
         private FlowLayoutPanel rightPanel;
         private C1.Win.Input.C1Label displayOptionTitle;
-        private C1.Win.Input.C1Label sampleDescription;
-        private C1.Win.Input.C1Label sampleTitle;
         private C1.Win.Input.C1CheckBox showHighlightedRowCheckbox;
         private C1.Win.Input.C1CheckBox showSelectedRowCheckbox;
         private C1.Win.Input.C1CheckBox showFocusedRowCheckbox;
